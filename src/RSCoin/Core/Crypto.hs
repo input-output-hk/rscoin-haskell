27c3e653687e9365bdc5feec5e7a390c3c6a9e40
Ante Kegalj <akegalj@gmail.com>
2016-03-22 22:32:03 +0100

    Add dummy crypto module

---
+++ src/RSCoin/Core/Crypto.hs
@@ -0,0 +1,39 @@
+-- | A small module providing necessary cryptographic functions
+module RSCoin.Core.Crypto
+       ( Hash
+       , getHash
+       , Signature
+       , getSignature
+       , SecretKey
+       , PrivateKey
+       , hash
+       , sign
+       , verify
+       , keyGen
+       ) where
+
+import           Data.ByteString        (ByteString)
+import qualified Data.ByteString.Base64 as B64
+import           Data.Binary            (Binary, encode)
+import qualified Crypto.Hash.SHA256     as SHA256
+
+import           OpenSSL.EVP.PKey       (SomeKeyPair, SomePublicKey)
+
+newtype Hash = Hash { getHash :: ByteString }
+newtype Signature = Signature { getSignature :: ByteString }
+
+newtype SecretKey  = SecretKey { getSecretKey :: SomeKeyPair }
+newtype PrivateKey = PrivateKey { getPrivateKey :: SomePublicKey }
+
+-- | Gets something serializable and gives back hash of it.
+hash :: Binary t => t -> Hash
+hash = Hash . B64.encode . SHA256.hashlazy . encode
+
+sign :: Binary t => SecretKey -> t -> IO Signature
+sign = undefined
+
+verify :: Binary t => PrivateKey -> t -> Signature -> IO Bool
+verify = undefined
+
+keyGen :: IO (SecretKey, PrivateKey)
+keyGen = undefined

c45d56ce849e1be7513ce62c831711cae20d53f8
Ivan Gromakovsky <gie1994@gmail.com>
2016-03-23 22:54:25 +0300

    Re-export Core

---
+++ src/RSCoin/Core.hs
@@ -0,0 +1,10 @@
+-- | Re-export RSCoin.Core.*
+
+module RSCoin.Core
+       (
+         module Exports
+       ) where
+
+import           RSCoin.Crypto     as Exports
+import           RSCoin.Primitives as Exports
+import           RSCoin.Types      as Exports

489fe8f1db99cdbdbfed33a4b48fd04ef22a3cb5
Ivan Gromakovsky <gie1994@gmail.com>
2016-03-25 23:49:55 +0300

    Add LBLock module

--- src/RSCoin/Core.hs
+++ src/RSCoin/Core.hs
@@ -8,6 +8,7 @@ module RSCoin.Core
 import           RSCoin.Core.Constants  as Exports
 import           RSCoin.Core.Crypto     as Exports
 import           RSCoin.Core.HBlock     as Exports
+import           RSCoin.Core.LBlock     as Exports
 import           RSCoin.Core.Owners     as Exports
 import           RSCoin.Core.Primitives as Exports
 import           RSCoin.Core.Types      as Exports

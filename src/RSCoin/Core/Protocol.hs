85a53a6c5b1c319af5e8cd46004cbb0bb58248f6
Ante Kegalj <akegalj@gmail.com>
2016-03-26 23:24:27 +0100

    Add serve handler

--- src/RSCoin/Core/Protocol.hs
+++ src/RSCoin/Core/Protocol.hs
@@ -5,9 +5,19 @@ module RSCoin.Core.Protocol
        ) where
 
 import           Network.JsonRpc   (FromRequest (parseParams), ToRequest (..),
-                                    FromResponse (parseResult))
+                                    FromResponse (parseResult), Ver (V2),
+                                    Respond, JsonRpcT, jsonRpcTcpServer,
+                                    receiveBatchRequest, BatchRequest (..),
+                                    sendResponse, buildResponse,
+                                    BatchResponse (..), sendBatchResponse)
+
+import           Control.Monad      (forM, liftM)
+import           Control.Monad.Logger (logDebug, MonadLoggerIO, runStderrLoggingT)
 import           Data.Aeson        (FromJSON (parseJSON), ToJSON (toJSON))
 import           Data.Aeson.Types  (emptyArray)
+import           Data.Conduit.Network (serverSettings)
+import           Data.Foldable     (forM_)
+import           Data.Maybe        (catMaybes)
 
 import           RSCoin.Core.Types (PeriodId, PeriodResult, Mintettes)
 import           RSCoin.Core.Aeson ()
@@ -46,3 +56,30 @@ instance FromResponse BankRes where
 instance ToJSON BankRes where
     toJSON (ResPeriodFinished pr) = toJSON pr
     toJSON (ResGetMintettes ms) = toJSON ms
+
+respond :: MonadLoggerIO m => Respond BankReq m BankRes
+respond = undefined
+
+serve :: Int -> IO ()
+serve port = runStderrLoggingT $ do
+    let ss = serverSettings port "::1"
+    jsonRpcTcpServer V2 False ss srv
+
+srv :: MonadLoggerIO m => JsonRpcT m ()
+srv = do
+    $(logDebug) "listening for new request"
+    qM <- receiveBatchRequest
+    case qM of
+        Nothing -> do
+            $(logDebug) "closed request channel, exting"
+            return ()
+        Just (SingleRequest q) -> do
+            $(logDebug) "got request"
+            rM <- buildResponse respond q
+            forM_ rM sendResponse
+            srv
+        Just (BatchRequest qs) -> do
+            $(logDebug) "got request batch"
+            rs <- catMaybes `liftM` forM qs (buildResponse respond)
+            sendBatchResponse $ BatchResponse rs
+            srv

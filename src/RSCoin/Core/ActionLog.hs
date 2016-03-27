165383e90ecbd6eec867fb4a46ed2d4d3b52afb8
Ivan Gromakovsky <gie1994@gmail.com>
2016-03-28 01:36:08 +0300

    Add actionLogNext function to ActionLog module

--- src/RSCoin/Core/ActionLog.hs
+++ src/RSCoin/Core/ActionLog.hs
@@ -2,6 +2,7 @@
 
 module RSCoin.Core.ActionLog
        ( checkActionLog
+       , actionLogNext
        ) where
 
 import           RSCoin.Core.Crypto (Hash, hash)
@@ -10,6 +11,7 @@ import           RSCoin.Core.Types  (ActionLog, ActionLogEntry)
 initialHash :: Hash
 initialHash = hash ("Ivan" :: String)
 
+-- | Check action log integrity using optional preceding element in log.
 checkActionLog :: Maybe (ActionLogEntry, Hash) -> ActionLog -> Bool
 checkActionLog start entries = all check [0 .. length entries - 1]
   where
@@ -19,3 +21,11 @@ checkActionLog start entries = all check [0 .. length entries - 1]
     check i
       | i == length entries - 1 = ithHash i == hash (ithEntry i, prevHash)
       | otherwise = ithHash i == hash (ithEntry i, ithHash (i + 1))
+
+-- | Generate next element in ActionLog using entry itself
+-- and optional previous element.
+actionLogNext :: Maybe (ActionLogEntry, Hash)
+              -> ActionLogEntry
+              -> (ActionLogEntry, Hash)
+actionLogNext prevHead entry = (entry, hash (entry, prevHash))
+  where prevHash = maybe initialHash snd prevHead

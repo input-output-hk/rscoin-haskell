0e825c5a97b3d8a28eccb726b49842e6ba3feb9f
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-03-29 10:28:36 +0200

    Update initBankUser.sh

--- admin/initBankUser.sh
+++ admin/initBankUser.sh
@@ -1,3 +1,3 @@
-stack $NIX_STACK exec rscoin-user -- --bank-mode update-blockchain
-stack $NIX_STACK exec rscoin-user -- --bank-mode list-addresses
+stack $NIX_STACK exec rscoin-user -- --bank-mode update
+stack $NIX_STACK exec rscoin-user -- --bank-mode list
 

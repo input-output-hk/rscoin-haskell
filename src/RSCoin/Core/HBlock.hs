bbc7627932c3de3dd257238c283d26d4f52a7b16
Ivan Gromakovsky <gie1994@gmail.com>
2016-03-29 23:50:58 +0300

    Implement checkHBlock

--- src/RSCoin/Core/HBlock.hs
+++ src/RSCoin/Core/HBlock.hs
@@ -7,7 +7,8 @@ module RSCoin.Core.HBlock
        ) where
 
 import           RSCoin.Core.Constants  (genesisAddress, genesisValue)
-import           RSCoin.Core.Crypto     (Hash, PublicKey, SecretKey, hash, sign)
+import           RSCoin.Core.Crypto     (Hash, PublicKey, SecretKey, hash, sign,
+                                         verify)
 import           RSCoin.Core.Primitives (Transaction (..))
 import           RSCoin.Core.Types      (Dpk, HBlock (..))
 
@@ -31,12 +32,22 @@ mkGenesisHBlock :: SecretKey -> Dpk -> HBlock
 mkGenesisHBlock sk dpk = mkHBlockDo [initialTx] sk dpk initialHash
 
 mkHBlockDo :: [Transaction] -> SecretKey -> Dpk -> Hash -> HBlock
-mkHBlockDo hbTransactions sk hbDpk hbHash =
+mkHBlockDo hbTransactions sk hbDpk prevHash =
     HBlock { .. }
   where
+    hbHash = hash (prevHash, hbTransactions)
     hbSignature = sign sk hbHash
 
 -- | Check that higher-level block is valid using Bank's public key
 -- and previous block (unless it's genesis block).
 checkHBlock :: PublicKey -> Maybe HBlock -> HBlock -> Bool
-checkHBlock = undefined
+checkHBlock pk Nothing blk = checkHBlockDo pk initialHash blk
+checkHBlock pk (Just b) blk = checkHBlockDo pk (hbHash b) blk
+
+checkHBlockDo :: PublicKey -> Hash -> HBlock -> Bool
+checkHBlockDo pk prevHash HBlock{..} =
+    and (checkHash : checkSignature : map checkDpk hbDpk)
+  where
+    checkHash = hbHash == hash (prevHash, hbTransactions)
+    checkSignature = verify pk hbSignature hbHash
+    checkDpk (mPk,signature) = verify pk signature mPk

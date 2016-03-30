4962fa45fdb33251db43f3ae73a44b6e7c9bc87e
Ante Kegalj <akegalj@gmail.com>
2016-03-30 12:39:00 +0200

    Add comments to protocol

--- src/RSCoin/Core/Protocol.hs
+++ src/RSCoin/Core/Protocol.hs
@@ -38,17 +38,20 @@ import           RSCoin.Core.Aeson          ()
 
 -- TODO: this module should provide more safety and expose better api
 
+-- | Requests used in RSCoin transport layer.
 data RSCoinMethod
     = RSCBank BankMethod
     | RSCMintette MintetteMethod
     deriving (Show)
 
+-- | Requests processed by a Bank.
 data BankMethod
     = GetMintettes
     | GetBlockchainHeight
     | GetHBlock
     deriving (Show)
 
+-- | Requests processed by a Mintette.
 data MintetteMethod
     = PeriodFinished
     | AnnounceNewPeriod
@@ -58,36 +61,44 @@ data MintetteMethod
 
 type Server a = S.Server (AsMessagePack a)
 
+-- | Create server method.
 method :: S.MethodType m f => RSCoinMethod -> f -> S.Method m
 method m = S.method (show m)
 
 --call :: RpcType a => RSCoinMethod -> a
 -- FIXME: RpcType isn't exported so my idea of using Show RSCoinMethod for method name
 -- doesn't hold any more
+-- | Call RSCoinMethod.
 call m = C.call (show m)
 
 -- TODO: this can be modeled with Cont monad
--- https://en.wikibooks.org/wiki/Haskell/Continuation_passing_style
+-- | Continuation passing style transformation.
+-- For more see: https://en.wikibooks.org/wiki/Haskell/Continuation_passing_style
 type WithResult a = (a -> IO ()) -> IO ()
 
+-- | Send a request to a Bank using Continuation passing style (CPS).
 execBank :: C.Client (AsMessagePack a) -> WithResult a
 execBank action withResult =
     C.execClient (BS.pack bankHost) bankPort $ do
         ret <- getAsMessagePack <$> action
         liftIO $ withResult ret
 
+-- | Send a request to a Mintette using Continuation passing style (CPS).
 execMintette :: Mintette -> C.Client (AsMessagePack a) -> WithResult a
 execMintette Mintette {..} action withResult =
     C.execClient (BS.pack mintetteHost) mintettePort $ do
         ret <- getAsMessagePack <$> action
         liftIO $ withResult ret
 
+-- | Send a request to a Bank.
 callBank :: C.Client (AsMessagePack a) -> IO a
 callBank = unCps . execBank
 
+-- | Send a request to a Mintette.
 callMintette :: Mintette -> C.Client (AsMessagePack a) -> IO a
 callMintette m = unCps . execMintette m
 
+-- | Reverse Continuation passing style (CPS) transformation
 unCps :: WithResult a -> IO a
 unCps withResult = do
     ref <- newIORef Nothing

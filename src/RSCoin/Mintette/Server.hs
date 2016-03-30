3aa23dbeeb7ab3564f7daadfa572609e856f11d6
Ivan Gromakovsky <gie1994@gmail.com>
2016-03-30 22:49:47 +0300

    Add some logging to Mintette

--- src/RSCoin/Mintette/Server.hs
+++ src/RSCoin/Mintette/Server.hs
@@ -6,12 +6,13 @@ module RSCoin.Mintette.Server
        ( serve
        ) where
 
-import           Control.Exception         (bracket, try)
-import           Control.Monad.Trans       (lift)
+import           Control.Exception         (bracket, catch, throwIO, try)
+import           Control.Monad.IO.Class    (liftIO)
 import           Data.Acid.Advanced        (update')
 import           Data.Text                 (Text)
 
-import           Serokell.Util.Text        (show')
+import           Serokell.Util.Text        (format', formatSingle',
+                                            listBuilderJSONIndent, show')
 
 import qualified RSCoin.Core               as C
 import           RSCoin.Mintette.AcidState (CheckNotDoubleSpent (..),
@@ -33,15 +34,36 @@ serve port dbPath sk =
                 , C.method (C.RSCMintette C.CommitTx) $ handleCommitTx sk st
                 ]
 
+toServer :: IO a -> C.Server a
+toServer action = liftIO $ action `catch` handler
+  where
+    handler (e :: MintetteError) = do
+        C.logError $ show' e
+        throwIO e
+
 handlePeriodFinished
     :: C.SecretKey -> State -> C.PeriodId -> C.Server C.PeriodResult
-handlePeriodFinished sk st pId = update' st $ FinishPeriod sk pId
+handlePeriodFinished sk st pId =
+    toServer $
+    do C.logInfo $ formatSingle' "Period {} has just finished!" pId
+       res@(_,blks,lgs) <- update' st $ FinishPeriod sk pId
+       C.logInfo $
+           format'
+               "Here is PeriodResult:\n Blocks: {}\n Logs: {}\n"
+               (listBuilderJSONIndent 2 blks, lgs)
+       return res
 
 handleNewPeriod :: State
                 -> C.MintetteId
                 -> C.NewPeriodData
                 -> C.Server ()
-handleNewPeriod st mId npd = update' st $ StartPeriod (mId, npd)
+handleNewPeriod st mId npd =
+    toServer $
+    do C.logInfo $
+           format'
+               "New period has just started, I am mintette {}. Here is new period data:\n {}"
+               (mId, npd)
+       update' st $ StartPeriod (mId, npd)
 
 handleCheckTx
     :: C.SecretKey
@@ -50,10 +72,11 @@ handleCheckTx
     -> C.AddrId
     -> C.Signature
     -> C.Server (Either Text C.CheckConfirmation)
-handleCheckTx sk st tx addrId sg = lift $ do
-    (res :: Either MintetteError C.CheckConfirmation) <-
-        try $ update' st $ CheckNotDoubleSpent sk tx addrId sg
-    either (return . Left . show') (return . Right) res
+handleCheckTx sk st tx addrId sg =
+    toServer $
+    do (res :: Either MintetteError C.CheckConfirmation) <-
+           try $ update' st $ CheckNotDoubleSpent sk tx addrId sg
+       either (return . Left . show') (return . Right) res
 
 handleCommitTx
     :: C.SecretKey
@@ -63,7 +86,7 @@ handleCommitTx
     -> C.CheckConfirmations
     -> C.Server (Either Text C.CommitConfirmation)
 handleCommitTx sk st tx pId cc =
-    lift $
+    toServer $
     do (res :: Either MintetteError C.CommitConfirmation) <-
            try $ update' st $ CommitTx sk tx pId cc
        either (return . Left . show') (return . Right) res

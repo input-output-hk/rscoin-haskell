728eda7494382272d778a559602bbc2a5686c2be
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-03-30 22:46:25 +0300

    Add debug log severity to runMintette.sh.

--- admin/runAsMintette.sh
+++ admin/runAsMintette.sh
@@ -8,5 +8,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 stack $NIX_STACK exec -- rscoin-bank --log-severity Debug add-mintette --host localhost --port 311$1 --key $(cat $dir/mintetteKeys/mintette$1/mintette$1.pub)
 cd $dir/mintetteKeys/mintette$1/ 
-stack $NIX_STACK exec -- rscoin-user --sk $dir/mintetteKeys/mintette$1/mintette$1.sec -p 311$1
+stack $NIX_STACK exec -- rscoin-mintette --log-severity Debug --sk $dir/mintetteKeys/mintette$1/mintette$1.sec -p 311$1
 

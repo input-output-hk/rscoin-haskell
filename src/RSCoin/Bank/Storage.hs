d5260511d9be6f569a3b78fa71d7e95603d3c51e
Ivan Gromakovsky <gie1994@gmail.com>
2016-04-01 13:36:16 +0300

    Fix LBlock checking on Bank side

--- src/RSCoin/Bank/Storage.hs
+++ src/RSCoin/Bank/Storage.hs
@@ -28,16 +28,18 @@ import           Control.Monad.Catch       (MonadThrow (throwM))
 import           Control.Monad.State.Class (MonadState)
 import qualified Data.HashMap.Lazy         as M
 import qualified Data.HashSet              as S
-import           Data.List                 ((\\))
+import           Data.List                 (unfoldr, (\\))
 import qualified Data.Map                  as MP
 import           Data.Maybe                (mapMaybe)
 import           Data.Typeable             (Typeable)
 import           Debug.Trace               (trace)
 import           Safe                      (atMay, headMay)
 
-import           RSCoin.Core               (ActionLog, AddrId, Address (..),
-                                            Coin (..), Dpk, HBlock (..),
-                                            Mintette, MintetteId, Mintettes,
+import           RSCoin.Core               (ActionLog,
+                                            ActionLogEntry (CloseEpochEntry),
+                                            AddrId, Address (..), Coin (..),
+                                            Dpk, HBlock (..), Mintette,
+                                            MintetteId, Mintettes,
                                             NewPeriodData (..), PeriodId,
                                             PeriodResult, PublicKey, SecretKey,
                                             Transaction (..), Utxo,
@@ -186,17 +188,32 @@ checkResult :: PeriodId
             -> HBlock
             -> (Maybe PeriodResult, PublicKey, ActionLog)
             -> Maybe PeriodResult
-checkResult expectedPid lastHBlock (r, key, storedLog) = do
-    (pId, lBlocks, actionLog) <- r
+checkResult expectedPid lastHBlock (r,key,storedLog) = do
+    (pId,lBlocks,actionLog) <- r
     let g1 = guard $ pId == expectedPid
     let g2 = guard $ checkActionLog (headMay storedLog) actionLog
     trace ("Guard 1 is " ++ show (pId == expectedPid)) $ return ()
-    trace ("Guard 2 is " ++ show (checkActionLog (headMay storedLog) actionLog)) $ return ()
+    trace
+        ("Guard 2 is " ++ show (checkActionLog (headMay storedLog) actionLog)) $
+        return ()
     g1
     g2
-    mapM_ (guard . checkLBlock key (hbHash lastHBlock) actionLog) lBlocks
+    let logsToCheck = formLogsToCheck actionLog
+    let g3 = length logsToCheck == length lBlocks
+    trace ("Guard 3 is " ++ show g3) $ guard g3
+    mapM_
+        (\(blk,lg) ->
+              guard $ checkLBlock key (hbHash lastHBlock) lg blk) $
+        zip lBlocks (formLogsToCheck actionLog)
     trace "Passed guards 1-3" $ return ()
     r
+  where
+    formLogsToCheck = unfoldr step
+    step [] = Nothing
+    step actionLog = Just (actionLog, dropEpoch actionLog)
+    dropEpoch = dropWhile (not . isCloseEpoch) . drop 1
+    isCloseEpoch (CloseEpochEntry _,_) = True
+    isCloseEpoch _ = False
 
 allocateCoins :: PublicKey
               -> [PublicKey]

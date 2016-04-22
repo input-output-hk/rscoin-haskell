0710b9210234568c224ea3a58c73d4770e7917fa
Ante Kegalj <akegalj@gmail.com>
2016-04-22 14:31:05 +0200

    Refactor, prepere for TimedT testing

--- test/RSCoin/Test/MonadTimedSpec.hs
+++ test/RSCoin/Test/MonadTimedSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE Rank2Types                #-}
 {-# LANGUAGE TypeSynonymInstances      #-}
 
 -- | RSCoin.Test.MonadTimed specification
@@ -15,86 +16,119 @@ import           Test.Hspec                  (Spec, describe)
 import           Test.Hspec.QuickCheck       (prop)
 import           Test.QuickCheck             (Arbitrary (arbitrary), Gen, oneof,
                                              Property, ioProperty, (.&&.))
+import           Test.QuickCheck.Monadic     (run, assert, PropertyM, monadic)
 import           Test.QuickCheck.Poly        (A)
 
 import           RSCoin.Test.MonadTimed      (RelativeToNow, MicroSeconds, now,
                                               after, MonadTimed (..), runTimedIO,
                                               TimedIO, schedule, invoke)
+import           RSCoin.Test.Timed           (runTimedT, TimedT)
 
 spec :: Spec
 spec =
-    describe "MonadTimed" $do
-        describe "now" $ do
-            prop "should return the same time as specified" nowProp
-        describe "localTime >> localTime" $ do
-            prop "first localTime will run before second localTime" localTimePassingProp
-        describe "wait t" $ do
-            prop "will wait at least t" waitPassingProp
-        describe "fork" $ do
-            prop "won't change semantics of an action" forkSemanticProp
-        describe "schedule" $ do
-            prop "won't change semantics of an action, will execute action in the future" scheduleSemanticProp
-        describe "invoke" $ do
-            prop "won't change semantics of an action, will execute action in the future" invokeSemanticProp
+    describe "MonadTimed" $ do
+        describe "TimedIO" $ do
+            describe "now" $ do
+                prop "should return the same time as specified" 
+                    nowProp
+            describe "localTime >> localTime" $ do
+                prop "first localTime will run before second localTime" $
+                    runTimedIOProp localTimePassingProp
+            describe "wait t" $ do
+                prop "will wait at least t" $
+                    runTimedIOProp . waitPassingProp
+            describe "fork" $ do
+                prop "won't change semantics of an action" $
+                    \a -> runTimedIOProp . forkSemanticProp a
+            describe "schedule" $ do
+                prop "won't change semantics of an action, will execute action in the future" $
+                    \a b -> runTimedIOProp . scheduleSemanticProp a b
+            describe "invoke" $ do
+                prop "won't change semantics of an action, will execute action in the future" $
+                    \a b -> runTimedIOProp . invokeSemanticProp a b
 
 instance Show (a -> a) where
     show = const "Dummy method show"
 
 -- TODO: figure out how to test recursive functions like after/at
 
+runTimedIOProp :: PropertyM TimedIO () -> Property
+runTimedIOProp = monadic $ ioProperty . runTimedIO
+
+-- runTimedTProp :: PropertyM (TimedT IO) () -> Property
+-- runTimedTProp = monadic $ ioProperty . runTimedT
+
 invokeSemanticProp
-    :: RelativeToNow
+    :: (MonadTimed m, MonadIO m)
+    => RelativeToNow
     -> A
     -> (A -> A)
-    -> Property
+    -> PropertyM m ()
 invokeSemanticProp = actionTimeSemanticProp invoke
 
 scheduleSemanticProp
-    :: RelativeToNow
+    :: (MonadTimed m, MonadIO m)
+    => RelativeToNow
     -> A
     -> (A -> A)
-    -> Property
+    -> PropertyM m ()
 scheduleSemanticProp = actionTimeSemanticProp schedule
 
 actionTimeSemanticProp
-    :: (RelativeToNow -> TimedIO () -> TimedIO ())
+    :: (MonadTimed m, MonadIO m)
+    => (RelativeToNow -> m () -> m ())
     -> RelativeToNow
     -> A
     -> (A -> A)
-    -> Property
-actionTimeSemanticProp action relativeToNow init modify =
-         actionSemanticProp action' init modify  
-    .&&. timePassingProp relativeToNow (action' $ pure ())
+    -> PropertyM m ()
+actionTimeSemanticProp action relativeToNow init modify = do
+    actionSemanticProp action' init modify  
+    timePassingProp relativeToNow (action' $ pure ())
   where
     action' = action relativeToNow
 
-forkSemanticProp :: A -> (A -> A) -> Property
-forkSemanticProp = actionSemanticProp fork
+forkSemanticProp
+    :: (MonadTimed m, MonadIO m)
+    => A
+    -> (A -> A)
+    -> PropertyM m ()
+forkSemanticProp = actionSemanticProp $ fork
 
-waitPassingProp :: RelativeToNow -> Property
+waitPassingProp
+    :: (MonadTimed m, MonadIO m)
+    => RelativeToNow
+    -> PropertyM m ()
 waitPassingProp relativeToNow =
     timePassingProp relativeToNow $ wait relativeToNow
 
-localTimePassingProp :: Property
+localTimePassingProp :: (MonadTimed m, MonadIO m) => PropertyM m ()
 localTimePassingProp =
     timePassingProp now $ pure ()
 
-timePassingProp :: RelativeToNow -> TimedIO () -> Property
-timePassingProp relativeToNow action = ioProperty . runTimedIO $ do
-    t1 <- localTime
-    action
-    t2 <- localTime
-    return $ relativeToNow t1 <= t2
+-- | Proves that at least relativeToNow has passed while executing an action
+timePassingProp
+    :: (MonadTimed m, MonadIO m)
+    => RelativeToNow
+    -> m ()
+    -> PropertyM m ()
+timePassingProp relativeToNow action = do
+    t1 <- run localTime
+    run action
+    t2 <- run localTime
+    assert $ relativeToNow t1 <= t2
 
+-- | Proves that an action will be executed
 actionSemanticProp
-    :: (TimedIO () -> TimedIO ())
+    :: (MonadTimed m, MonadIO m)
+    => (m () -> m ())
     -> A
     -> (A -> A)
-    -> Property
-actionSemanticProp action init modify = ioProperty . runTimedIO $ do
+    -> PropertyM m ()
+actionSemanticProp action init modify = do
     tvar <- liftIO $ newTVarIO init
-    action $ liftIO $ atomically $ modifyTVar tvar modify
-    liftIO $ (modify init ==) <$> readTVarIO tvar
+    run . action . liftIO . atomically $ modifyTVar tvar modify
+    modvar <- liftIO $ readTVarIO tvar
+    assert $ modify init == modvar
 
 nowProp :: MicroSeconds -> Bool
 nowProp ms = ms == now ms

3191b3ac68b6d429218f38942cd693995b985338
Ante Kegalj <akegalj@gmail.com>
2016-04-26 13:52:34 +0200

    Simplify tests

--- test/RSCoin/Test/MonadRpcSpec.hs
+++ test/RSCoin/Test/MonadRpcSpec.hs
@@ -11,17 +11,14 @@ import           Control.Concurrent.Async    (race_)
 import           Control.Monad.Trans         (liftIO, lift, MonadIO)
 import           Data.MessagePack.Object     (Object(..), MessagePack, 
                                               toObject)
-import qualified Data.ByteString             as B
 import qualified Data.Map                    as M
-import qualified Data.Text                   as T
-import qualified Data.Vector                 as V
 import           Numeric.Natural             (Natural)
 import           Test.Hspec                  (Spec, describe)
 import           Test.Hspec.QuickCheck       (prop)
 import           Test.QuickCheck             (Arbitrary (arbitrary), 
                                              Property, ioProperty,
                                              counterexample, oneof, elements,
-                                             NonEmptyList (..))
+                                             NonEmptyList (..), Gen)
 import           Test.QuickCheck.Function    (Fun, apply) 
 import           Test.QuickCheck.Monadic     (run, assert, PropertyM, monadic,
                                              monitor, pick, forAllM)
@@ -50,31 +47,12 @@ msgPackRpcSpec description runProp =
         describe "server method should execute" $ do
             prop "client should be able to execute server method" $
                 runProp serverMethodShouldExecuteSimpleSpec
+        describe "server method should execute" $ do
+            prop "client should be able to execute server method" $
+                runProp . serverMethodShouldExecuteSpec
 
 runMsgPackRpcProp :: PropertyM MsgPackRpc () -> Property
 runMsgPackRpcProp = monadic $ ioProperty . runTimedIO . runMsgPackRpc
-
-instance Arbitrary Object where
-    arbitrary = oneof [ pure ObjectNil
-                      , ObjectBool <$> arbitrary
-                      , ObjectInt <$> arbitrary
-                      , ObjectFloat <$> arbitrary
-                      , ObjectDouble <$> arbitrary
-                      , ObjectStr <$> arbitrary
-                      , ObjectBin <$> arbitrary
-                      , ObjectArray <$> arbitrary
-                      , ObjectMap <$> arbitrary
-                      , ObjectExt <$> arbitrary <*> arbitrary
-                      ]
-
-instance Arbitrary T.Text where
-    arbitrary = T.pack <$> arbitrary
-
-instance Arbitrary B.ByteString where
-    arbitrary = B.pack <$> arbitrary
-
-instance Arbitrary a => Arbitrary (V.Vector a) where
-    arbitrary = V.fromList <$> arbitrary
 
 -- TODO: it would be useful to create an instance of Function for Client and Method;
 -- see here https://hackage.haskell.org/package/QuickCheck-2.8.2/docs/Test-QuickCheck-Function.html#t:Function
@@ -132,21 +110,21 @@ client = do
 -- | Method should execute if called correctly
 serverMethodShouldExecuteSpec
     :: (MonadTimed m, MonadRpc m)
-    => NonEmptyList (NonEmptyList Char, [Object])
+    => NonEmptyList (NonEmptyList Char)
     -> PropertyM m ()
-serverMethodShouldExecuteSpec (getNonEmpty -> methods') = do
-    mtds <- createMethods methods
+serverMethodShouldExecuteSpec (getNonEmpty -> methodNames') = do
+    mtds <- createMethods methodNames
     let methodMap = createMethodMap mtds
-    run . work (during $ ms 1000) $ serve port mtds
-    run . wait $ for 100 ms
-    (name, args) <- pick $ elements methods
-    res <- run . execClient addr $ Client name args
-    let shouldBe = M.lookup name methodMap <*> pure args
+    run . fork $ serve port mtds
+    run . wait $ for 500 ms
+    name <- pick $ elements methodNames
+    res <- run . execClient addr $ Client name []
+    let shouldBe = M.lookup name methodMap <*> pure []
     maybe (return ()) (\k -> assert . (== res) =<< run k) shouldBe
-  where methods = map (\(a, b) -> (getNonEmpty a, b)) methods'
+  where methodNames = map getNonEmpty methodNames'
         -- TODO: we wouldn't need to do this if Function was defined
-        createMethods :: Monad m => [(String, [Object])] -> PropertyM m [Method m]
-        createMethods = mapM $ \(name, args) -> do
-            res <- pick arbitrary
+        createMethods :: Monad m => [String] -> PropertyM m [Method m]
+        createMethods = mapM $ \name -> do
+            res <- toObject <$> pick (arbitrary :: Gen Int)
             return . Method name . const $ return res
         createMethodMap = M.fromList . map (\m -> (methodName m, methodBody m))

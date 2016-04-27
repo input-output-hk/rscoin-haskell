70c48ede589ff64d26b1439fea731017e94360ae
Ante Kegalj <akegalj@gmail.com>
2016-04-27 13:34:28 +0200

    Add TimedT tests

--- test/RSCoin/Test/MonadTimedSpec.hs
+++ test/RSCoin/Test/MonadTimedSpec.hs
@@ -10,7 +10,9 @@ module RSCoin.Test.MonadTimedSpec
        ) where
 
 import           Control.Concurrent.MVar  (newEmptyMVar, putMVar, takeMVar)
+import           Control.Monad.State      (State, execState, modify)
 import           Control.Monad.Trans      (MonadIO, liftIO)
+import           Control.Monad.Catch.Pure (CatchT, runCatchT)
 import           Numeric.Natural          (Natural)
 import           Test.Hspec               (Spec, describe)
 import           Test.Hspec.QuickCheck    (prop)
@@ -24,11 +26,16 @@ import           Test.QuickCheck.Poly     (A)
 import           RSCoin.Test.MonadTimed   (MicroSeconds, MonadTimed (..),
                                            RelativeToNow, invoke, now, schedule)
 import           RSCoin.Test.TimedIO      (TimedIO, runTimedIO)
+import           RSCoin.Test.Timed        (TimedT, runTimedT)
 
 spec :: Spec
 spec =
     describe "MonadTimed" $ do
+        describe "now" $ do
+            prop "should return the same time as specified"
+                nowProp
         monadTimedSpec "TimedIO" runTimedIOProp
+        monadTimedTSpec "TimedT" runTimedTProp
 
 monadTimedSpec
     :: (MonadTimed m, MonadIO m)
@@ -37,9 +44,6 @@ monadTimedSpec
     -> Spec
 monadTimedSpec description runProp =
     describe description $ do
-        describe "now" $ do
-            prop "should return the same time as specified"
-                nowProp
         describe "localTime >> localTime" $ do
             prop "first localTime will run before second localTime" $
                 runProp localTimePassingProp
@@ -56,6 +60,34 @@ monadTimedSpec description runProp =
             prop "won't change semantics of an action, will execute action in the future" $
                 \a b -> runProp . invokeSemanticProp a b
 
+monadTimedTSpec
+    :: String
+    -> (TimedTProp () -> Bool)
+    -> Spec
+monadTimedTSpec description runProp =
+    describe description $ do
+        describe "localTime >> localTime" $ do
+            prop "first localTime will run before second localTime" $
+                runProp localTimePassingTimedProp
+        describe "wait t" $ do
+            prop "will wait at least t" $
+                runProp . waitPassingTimedProp
+        describe "fork" $ do
+            prop "won't change semantics of an action" $
+                \a -> runProp . forkSemanticTimedProp a
+        describe "schedule" $ do
+            prop "won't change semantics of an action, will execute action in the future" $
+                \a b -> runProp . scheduleSemanticTimedProp a b
+        describe "invoke" $ do
+            prop "won't change semantics of an action, will execute action in the future" $
+                \a b -> runProp . invokeSemanticTimedProp a b
+
+
+type TimedTProp = TimedT (CatchT (State Bool))
+
+assertTimedT :: Bool -> TimedTProp ()
+assertTimedT b = modify (b &&)
+
 type RelativeToNowNat = Natural
 
 fromIntegralRTN :: RelativeToNowNat -> RelativeToNow
@@ -66,8 +98,10 @@ fromIntegralRTN = (+) . fromIntegral
 runTimedIOProp :: PropertyM TimedIO () -> Property
 runTimedIOProp = monadic $ ioProperty . runTimedIO
 
--- runTimedTProp :: PropertyM (TimedT IO) () -> Property
--- runTimedTProp = monadic $ ioProperty . runTimedT
+runTimedTProp :: TimedTProp () -> Bool
+runTimedTProp test = execState (runCatchT $ runTimedT test) True
+
+-- TimedIO tests
 
 invokeSemanticProp
     :: (MonadTimed m, MonadIO m)
@@ -154,6 +188,69 @@ actionSemanticProp action val f = do
         , ", should be: ", show result
         ])
     assert $ apply f val == result
+
+-- TimedT tests
+
+invokeSemanticTimedProp
+    :: RelativeToNowNat
+    -> A
+    -> Fun A A
+    -> TimedTProp ()
+invokeSemanticTimedProp = actionTimeSemanticTimedProp invoke
+
+scheduleSemanticTimedProp
+    :: RelativeToNowNat
+    -> A
+    -> Fun A A
+    -> TimedTProp ()
+scheduleSemanticTimedProp = actionTimeSemanticTimedProp schedule
+
+actionTimeSemanticTimedProp
+    :: (RelativeToNow -> TimedTProp () -> TimedTProp ())
+    -> RelativeToNowNat
+    -> A
+    -> Fun A A
+    -> TimedTProp ()
+actionTimeSemanticTimedProp action relativeToNow val f = do
+    actionSemanticTimedProp action' val f
+    timePassingTimedProp relativeToNow action'
+  where
+    action' = action $ fromIntegralRTN relativeToNow
+
+forkSemanticTimedProp
+    :: A
+    -> Fun A A
+    -> TimedTProp ()
+forkSemanticTimedProp = actionSemanticTimedProp fork
+
+waitPassingTimedProp
+    :: RelativeToNowNat
+    -> TimedTProp ()
+waitPassingTimedProp relativeToNow =
+    timePassingTimedProp relativeToNow (wait (fromIntegralRTN relativeToNow) >>)
+
+localTimePassingTimedProp :: TimedTProp ()
+localTimePassingTimedProp =
+    timePassingTimedProp 0 id
+
+-- | Tests that action will be exececuted after relativeToNow
+timePassingTimedProp
+    :: RelativeToNowNat
+    -> (TimedTProp () -> TimedTProp ())
+    -> TimedTProp ()
+timePassingTimedProp relativeToNow action = do
+    t1 <- localTime
+    action $ localTime >>= assertTimedT . (fromIntegralRTN relativeToNow t1 <=)
+
+-- | Tests that an action will be executed
+actionSemanticTimedProp
+    :: (TimedTProp () -> TimedTProp ())
+    -> A
+    -> Fun A A
+    -> TimedTProp ()
+actionSemanticTimedProp action val f = do
+    let result = apply f val
+    action $ assertTimedT $ apply f val == result
 
 nowProp :: MicroSeconds -> Property
 nowProp ms = 0 === now ms

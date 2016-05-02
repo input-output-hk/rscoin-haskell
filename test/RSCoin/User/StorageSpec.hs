cc205b112b246bf6a4353b6c64911f2784a8c944
Ante Kegalj <akegalj@gmail.com>
2016-05-02 23:36:49 +0200

    Add user storage initial test module

---
+++ test/RSCoin/User/StorageSpec.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE TupleSections             #-}
+{-# LANGUAGE TypeSynonymInstances      #-}
+
+-- | HSpec specification of User's Storage.
+
+module RSCoin.User.StorageSpec
+       ( spec
+       , Update
+       , UpdateVoid
+       , UserState (..)
+       , userStorage
+       , userKey
+       ) where
+
+import           Control.Lens          (makeLenses)
+
+import           Test.Hspec            (Spec, describe)
+import           Test.QuickCheck       (Arbitrary (arbitrary), Gen, frequency)
+
+import qualified RSCoin.User.Error     as S
+import qualified RSCoin.User.Wallet    as S
+import qualified RSCoin.Core           as C
+
+import           RSCoin.Core.Arbitrary ()
+import qualified RSCoin.Core.Storage   as T
+
+spec :: Spec
+spec =
+    describe "User storage" $ do
+        return ()
+
+type Update = T.Update S.UserError S.WalletStorage
+type UpdateVoid = Update ()
+
+class CanUpdate a where
+    doUpdate :: a -> UpdateVoid
+
+data SomeUpdate = forall a . CanUpdate a => SomeUpdate a
+
+data EmptyUpdate = EmptyUpdate
+    deriving Show
+
+instance Arbitrary EmptyUpdate where
+    arbitrary = pure EmptyUpdate
+
+instance CanUpdate EmptyUpdate where
+    doUpdate _ = return ()
+
+instance Arbitrary SomeUpdate where
+    arbitrary =
+        frequency
+            [ (1, SomeUpdate <$> (arbitrary :: Gen EmptyUpdate))
+            ]
+
+data UserState = UserState
+    { _userStorage :: S.WalletStorage
+    , _userKey     :: C.SecretKey
+    }
+
+$(makeLenses ''UserState)
+
+instance Show UserState where
+  show = const "UserState"
+
+instance Arbitrary UserState where
+    arbitrary = do
+        sk <- arbitrary
+        SomeUpdate upd <- arbitrary
+        return . flip UserState sk $ T.execUpdate (doUpdate upd) S.emptyWalletStorage

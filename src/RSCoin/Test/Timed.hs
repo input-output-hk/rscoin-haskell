96cd2ffaac3bdaef501278af73ea861667ab8bfc
Martoon <Martoon_00@mail.ru>
2016-05-03 04:05:44 +0300

    Preparations for fixing TimedT

--- src/RSCoin/Test/Timed.hs
+++ src/RSCoin/Test/Timed.hs
@@ -15,8 +15,9 @@ module RSCoin.Test.Timed
 
 import           Control.Exception      (SomeException)
 import           Control.Exception.Base (AsyncException (ThreadKilled))
-import           Control.Monad.Catch    (throwM)
-import           Control.Lens           (makeLenses, to, use, (%=), (.=), (^.))
+import           Control.Monad.Catch    (Handler(..), throwM)
+import           Control.Lens           (makeLenses, to, use, (%=), (.=), (^.),
+                                         view, (|>), (&))
 import           Control.Monad          (void, unless)
 import           Control.Monad.Catch    (MonadCatch, MonadThrow, MonadMask, 
                                          catch, mask, uninterruptibleMask)
@@ -42,11 +43,26 @@ import           RSCoin.Core.Logging    (logWarning)
 
 type Timestamp = MicroSeconds
 
+-- | Private context for each pure thread
+data ThreadCtx m = ThreadCtx
+    { _condition :: m Bool          -- ^ Whether thread should remain alive
+    , _handlers  :: [Handler m ()]  -- ^ Exception handlers stack
+    }
+
+$(makeLenses ''ThreadCtx)
+
+initThreadCtx :: Monad m => ThreadCtx m
+initThreadCtx = 
+    ThreadCtx
+    { _condition = return True
+    , _handlers  = []
+    }
+
 -- | Timestamped action
 data Event m = Event
     { _timestamp :: Timestamp
     , _action    :: m ()
-    , _condition :: m Bool
+    , _threadCtx :: ThreadCtx m
     }
 
 $(makeLenses ''Event)
@@ -75,7 +91,7 @@ emptyScenario =
 -- | Pure implementation of MonadTimed.
 --   It stores an event queue, on wait continuation is passed to that queue
 newtype TimedT m a = TimedT
-    { unwrapTimedT :: ReaderT (TimedT m Bool)
+    { unwrapTimedT :: ReaderT (ThreadCtx (TimedT m))
                      (ContT ()
                      (StateT (Scenario (TimedT m)) m)) a
     } deriving (Functor, Applicative, Monad, MonadIO, MonadThrow)
@@ -92,22 +108,21 @@ instance MonadState s m => MonadState s (TimedT m) where
     put = lift . put
     state = lift . state
 
--- I don't understand why ConT monad is not an instance of MonadCatch
--- by default
 instance MonadCatch m => MonadCatch (TimedT m) where
     catch (TimedT m) handler =
         TimedT $
         ReaderT $
         \r ->
-             ContT $
-             \c ->
-                  catch (m' r c) (handler' r)
+            ContT $
+                \c ->
+                    catch (m' r c) (handler' r)
       where
         m' r c = runContT (runReaderT m r) c
         handler' r =
             flip runContT (return . const ()) .
             flip runReaderT r . unwrapTimedT . handler
 
+-- Posibly incorrect instance
 instance MonadMask m => MonadMask (TimedT m) where
     mask a = TimedT $ ReaderT $ \r -> ContT $ \c -> 
         mask $ \u -> runContT (runReaderT (unwrapTimedT $ a $ q u) r) c
@@ -126,11 +141,10 @@ instance MonadMask m => MonadMask (TimedT m) where
 launchTimedT :: Monad m => TimedT m a -> m ()
 launchTimedT (TimedT t) = flip evalStateT emptyScenario
                         $ flip runContT   (void . return)
-                        $ flip runReaderT (return True)
+                        $ flip runReaderT initThreadCtx
                         $ t
 
 -- | Starts timed evaluation. Finishes when no more scheduled actions remain.
--- FIXME:  MonadCatch is not necessary here, we just should catch if it can throw
 runTimedT :: (Monad m, MonadCatch m) => TimedT m () -> m ()
 runTimedT timed = launchTimedT $ do
     schedule now timed `catch` handler
@@ -141,14 +155,11 @@ runTimedT timed = launchTimedT $ do
             return ev
         TimedT $ curTime .= nextEv ^. timestamp
 
-        let cond = nextEv ^. condition
-        keepAlive <- cond
-        -- We can't just invoke (nextEv ^. action) here, because it can put
-        -- further execution to event queue or even throw it away.
-        -- We want to successfully finish this action and go to next iteration
-        -- rather than lose execution control
+        let ctx = nextEv ^. threadCtx 
+        keepAlive <- ctx ^. condition
         let TimedT act = nextEv ^. action
-            act'     = runContT (runReaderT act cond) return
+            -- put empty continuation to an action (not our own!)
+            act'     = runContT (runReaderT act ctx) return
             act''    = (maybeDie >> act') `catch` handler
             act'''   = TimedT . lift . lift $ act''
             maybeDie = unless keepAlive $ throwM ThreadKilled
@@ -157,27 +168,32 @@ runTimedT timed = launchTimedT $ do
     notDone :: Monad m => TimedT m Bool
     notDone = TimedT . use $ events . to (not . PQ.null)
 
-    {-# NOINLINE handler #-}
     handler :: Monad m => SomeException -> m ()
     handler e = let text = formatSingle' "Thread killed by exception: {}" $ 
                            T.pack . show $ e
                 in  return $! unsafePerformIO $ logWarning text
+    {-# NOINLINE handler #-}
 
 instance MonadThrow m => MonadTimed (TimedT m) where
     localTime = TimedT $ use curTime
 
     -- | Take note, created thread may be killed by timeout
     --   only when it calls "wait"
-    workWhile _condition _action = do
+    workWhile cond _action = do
         _timestamp <- localTime
+        let _threadCtx = 
+                ThreadCtx
+                { _condition = cond
+                , _handlers  = []
+                }
         TimedT $ events %= PQ.insert Event { .. }
 
     wait relativeToNow = do
         cur <- localTime
-        cond <- TimedT $ ask
+        ctx <- TimedT $ ask
         let event following =
                 Event
-                { _condition = cond
+                { _threadCtx = ctx
                 , _timestamp = cur + relativeToNow cur
                 , _action = TimedT $ lift . lift $ following ()
                 }

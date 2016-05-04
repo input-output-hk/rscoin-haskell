171b0f0b8d36f47df870fc08affba01297db0b30
Ante Kegalj <akegalj@gmail.com>
2016-05-04 18:30:46 +0200

    Almost finish formTransaction (validateTransaction left to implement) simulation

--- test/RSCoin/StorageSpec.hs
+++ test/RSCoin/StorageSpec.hs
@@ -15,19 +15,22 @@ module RSCoin.StorageSpec
        ) where
 
 import           Control.Lens              (ix, makeLenses, use,
-                                            (.=), at, preuse, to)
+                                            (.=), at, preuse, to, (^.))
 
-import           Control.Monad              (forM, when, forM_)
+import           Control.Monad              (forM, when, forM_, unless, filterM)
 import           Control.Monad.Trans        (lift)
 import           Control.Monad.Catch        (MonadThrow (throwM))
 import           Control.Monad.Reader       (MonadReader (ask, local))
 import           Control.Exception          (Exception)
 import           Control.Monad.State.Lazy   (gets)
-import           Data.Int              (Int64)
+import           Data.Int                   (Int64)
+import           Data.Function              (on)
+import           Data.List                  (nub, nubBy)
 import qualified Data.Map                   as M
-import           Data.Maybe                 (fromJust, fromMaybe)
+import           Data.Maybe                 (fromJust, fromMaybe, isJust)
 import           Data.Monoid                ((<>))
 import           Data.Text                  (Text)
+import           Data.Tuple.Select          (sel1)
 import           Data.Typeable              (Typeable)
 import           Serokell.Util.Text         (format', formatSingle')
 import           Test.Hspec                 (Spec, describe)
@@ -41,6 +44,7 @@ import qualified RSCoin.Bank.Storage     as B
 import qualified RSCoin.Mintette.Storage as M
 import qualified RSCoin.User.Error       as U
 import qualified RSCoin.User.Wallet      as U
+import qualified RSCoin.User.Logic       as U
 import qualified RSCoin.Core             as C
 
 import           RSCoin.Core.Arbitrary       ()
@@ -107,6 +111,7 @@ data StartNewPeriod = StartNewPeriod
 instance Arbitrary StartNewPeriod where
     arbitrary = pure StartNewPeriod
 
+-- NOTE: this is reimplementation of Bank.Worker
 instance CanUpdate StartNewPeriod where
     doUpdate _ = do
         mintettes <- use $ bankState . B.bankStorage . B.getMintettes
@@ -134,6 +139,7 @@ data UpdateToBlockHeight = UpdateToBlockHeight
 instance Arbitrary UpdateToBlockHeight where
     arbitrary = pure UpdateToBlockHeight
 
+-- NOTE: this is reimplementation of User.Actions.updateToBlockHeight
 instance CanUpdate UpdateToBlockHeight where
     doUpdate _ = do
         walletHeight <- liftUserUpdate U.getLastBlockId
@@ -168,6 +174,7 @@ data FormTransaction = FormTransaction InvalidAddress FromAddresses ToAddressInd
 instance Arbitrary FormTransaction where
     arbitrary = FormTransaction <$> arbitrary <*> arbitrary <*> arbitrary
 
+-- NOTE: this is reimplementation of User.Actions.formTransaction
 instance CanUpdate FormTransaction where
     doUpdate (FormTransaction invalidAddress (getNonEmpty -> fromIndexes) toIndex) = do
         -- Generating random transaction
@@ -179,10 +186,97 @@ instance CanUpdate FormTransaction where
             inputs = map (\(a, b) -> (getNonNegative a, getPositive b)) fromIndexes
             outputAddr = fromMaybe validAddress invalidAddress
             outputCoin = C.Coin . sum $ map snd inputs
-        return ()
+        when
+            (nubBy ((==) `on` fst) inputs /= inputs) $
+            commitError "All input addresses should have distinct IDs."
+        unless (all (> 0) $ map snd inputs) $
+            commitError $
+            formatSingle'
+                "All input values should be positive, but encountered {}, that's not." $
+            head $ filter (<= 0) $ map snd inputs
+        accounts <- liftUserUpdate U.getAllAddresses
+        let notInRange i = i <= 0 || i > length accounts
+        when
+            (any notInRange $ map fst inputs) $
+            commitError $
+            format'
+                "Found an account id ({}) that's not in [1..{}]"
+                ( head $ filter notInRange $ map fst inputs
+                , length accounts)
+        let accInputs :: [(Int, U.UserAddress, C.Coin)]
+            accInputs = map (\(i,c) -> (i, accounts !! (i - 1), C.Coin c)) inputs
+            hasEnoughFunds (i,acc,c) = liftUserUpdate $ do
+                amount <- getAmount acc
+                return $
+                    if amount >= c
+                        then Nothing
+                        else Just i
+        overSpentAccounts <-
+            filterM (\a -> isJust <$> hasEnoughFunds a) accInputs
+        unless (null overSpentAccounts) $
+            commitError $
+            (if length overSpentAccounts > 1
+                then "At least the"
+                else "The") <>
+            formatSingle'
+                " following account doesn't have enough coins: {}"
+                (sel1 $ head overSpentAccounts)
+        (addrPairList,outTr) <- liftUserUpdate $
+            foldl1 mergeTransactions <$> mapM (formTransactionMapper outputAddr outputCoin) accInputs
+        walletHeight <- liftUserUpdate U.getLastBlockId
+        lastBlockHeight <- use $ bankState . B.bankStorage . B.getPeriodId . to pred
+        when (walletHeight /= lastBlockHeight) $
+            commitError $
+            format'
+                ("Wallet isn't updated (lastBlockHeight {} when blockchain's last block is {}). " <>
+                "Please synchonize it with blockchain. The transaction wouldn't be sent.")
+                (walletHeight, lastBlockHeight)
+        let signatures =
+                M.fromList $
+                map (\(addrid',address') ->
+                        (addrid', C.sign (address' ^. U.privateAddress) outTr))
+                    addrPairList
+        validateTransaction outTr signatures $ lastBlockHeight + 1
+        liftUserUpdate $ U.addTemporaryTransaction outTr
       where
+        getAmount userAddress =
+            sum . map (C.getAmountByAddress $ U.toAddress userAddress) <$> U.getTransactions userAddress
+        commitError = throwM . U.InputProcessingError
         getAddress [] _ = error "This should not happen"
         getAddress list index = C.Address $ list !! (getNonNegative index `mod` length list)
+        formTransactionMapper :: C.Address
+                              -> C.Coin
+                              -> (Int, U.UserAddress, C.Coin)
+                              -> T.Update U.UserError U.WalletStorage ([(C.AddrId, U.UserAddress)], C.Transaction)
+        formTransactionMapper outputAddr outputCoin (_,a,c) = do
+            (addrids :: [C.AddrId]) <-
+                concatMap (C.getAddrIdByAddress $ U.toAddress a) <$>
+                U.getTransactions a
+            let (chosen,leftCoin) = C.chooseAddresses addrids c
+                transaction =
+                    C.Transaction chosen $
+                    (outputAddr, outputCoin) :
+                    (if leftCoin == 0
+                        then []
+                        else [(U.toAddress a, leftCoin)])
+                addrPairList = map (, a) chosen
+            return (addrPairList, transaction)
+        mergeTransactions
+            :: ([(C.AddrId, U.UserAddress)], C.Transaction)
+            -> ([(C.AddrId, U.UserAddress)], C.Transaction)
+            -> ([(C.AddrId, U.UserAddress)], C.Transaction)
+        mergeTransactions (s1,a) (s2,b) =
+            ( nub $ s1 <> s2
+            , C.Transaction
+                (C.txInputs a <> C.txInputs b)
+                (nub $ C.txOutputs a <> C.txOutputs b))
+
+validateTransaction :: C.Transaction -> M.Map C.AddrId C.Signature -> C.PeriodId -> UpdateVoid
+validateTransaction tx@C.Transaction{..} signatures height = do
+    undefined
+    -- FIXME: now I should reimplement everything from User.Logic module
+    -- there will be few more these functions, like Mintette.Worker, and maybe some other that I am not aware of. I don't like this approach, because lots of logic has been reimplemented. Ideally I should reuse these `WorkMode` functions and run them with pure monad stack `PureRpc` and `TimedT`. What do you think? Or should I just continue and reimplement User.Logic (plus some extra stuff from Core.Communication) and Mintette.Worker.
+
 instance Arbitrary SomeUpdate where
     arbitrary = 
         frequency

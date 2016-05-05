29cee6ae7666deb815213b5aaf291181a45d4391
Ante Kegalj <akegalj@gmail.com>
2016-05-05 18:20:25 +0200

    Add some user actions

--- test/Timed/Main.hs
+++ test/Timed/Main.hs
@@ -1,27 +1,33 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ViewPatterns              #-}
 
-import           Control.Lens         (view, (^.))
-import           Control.Monad.Trans  (MonadIO, liftIO)
-import           Control.Monad.Reader (runReaderT)
-import           Data.Acid            (update)
-import           Data.Default         (def)
-import           System.Random        (mkStdGen)
+import           Control.Lens          (view, (^.), preview, ix, to)
+import           Control.Monad.Trans   (MonadIO, liftIO)
+import           Control.Monad.Reader  (runReaderT)
+import           Data.Acid             (update, query)
+import           Data.Int              (Int64)
+import           Data.Default          (def)
+import           Data.Maybe            (fromJust)
+import           System.Random         (mkStdGen)
 
-import           Test.QuickCheck      (Arbitrary (arbitrary), NonNegative (..),
-                                       Gen, oneof)
+import           Test.QuickCheck       (Arbitrary (arbitrary), NonNegative (..),
+                                        Gen, oneof, Positive (..), NonEmptyList (..))
 
-import           RSCoin.Bank          as B
-import           RSCoin.Mintette      as M
-import           RSCoin.User          as U
-import           RSCoin.Core          (initLogging, Severity(Info), Mintette(..))
-import           RSCoin.Test          (WorkMode, runRealMode, runEmulationMode,
-                                       upto, mcs, work, minute, wait, for, sec,
-                                       interval, MicroSeconds)
-import           Context              (TestEnv, mkTestContext, state, port, 
-                                       keys, publicKey, secretKey, MintetteInfo,
-                                       bank, mintettes, lifetime, users, buser,
-                                       UserInfo, bankSkPath)
+import qualified  RSCoin.Bank          as B
+import qualified  RSCoin.Mintette      as M
+import qualified  RSCoin.User          as U
+import qualified  Actions              as U
+import qualified  UserOptions          as U
+import            RSCoin.Core          (initLogging, Severity(Info), Mintette(..),
+                                        Address)
+import           RSCoin.Test           (WorkMode, runRealMode, runEmulationMode,
+                                        upto, mcs, work, minute, wait, for, sec,
+                                        interval, MicroSeconds)
+import           RSCoin.Core.Arbitrary ()
+import           Context               (TestEnv, mkTestContext, state, port, 
+                                        keys, publicKey, secretKey, MintetteInfo,
+                                        bank, mintettes, lifetime, users, buser,
+                                        UserInfo, bankSkPath)
 
 class Action a where
     doAction :: WorkMode m => a -> TestEnv m ()
@@ -41,15 +47,66 @@ data WaitAction = WaitAction (NonNegative MicroSeconds)
     deriving Show
 
 instance Action WaitAction where
-    doAction (WaitAction (getNonNegative -> time)) = wait $ for time mcs
+    doAction (WaitAction (getNonNegative -> time)) =
+        wait $ for time mcs
 
 instance Arbitrary WaitAction where
     arbitrary = WaitAction <$> arbitrary
 
+-- | Nothing represents bank user, otherwise user is selected according
+-- to index in the list
+type UserIndex = Maybe (NonNegative Int)
+
+type InvalidAddress = Maybe Address
+type ValidAddressIndex = NonNegative Int
+type ToAddress = (UserIndex, InvalidAddress, ValidAddressIndex)
+type FromAddresses = NonEmptyList (ValidAddressIndex, Positive Int64)
+
+
+-- data DumpAction
+
+data UserAction
+    = ListAddresses UserIndex
+    | FormTransaction UserIndex FromAddresses ToAddress
+    | UpdateBlockchain UserIndex
+   -- TODO: we use dumping only for debug but we should cover all cases
+   -- | Dump DumpAction
+    deriving Show
+
+instance Arbitrary UserAction where
+    arbitrary =
+        oneof [ ListAddresses <$> arbitrary
+              , FormTransaction <$> arbitrary <*> arbitrary <*> arbitrary
+              , UpdateBlockchain <$> arbitrary
+              ]
+
+instance Action UserAction where
+    doAction (ListAddresses userIndex) =
+        runUserAction userIndex U.ListAddresses
+    doAction (FormTransaction userIndex fromAddresses toAddress) = do
+        publicAddresses <- getUser userIndex >>= liftIO . flip query U.GetPublicAddresses
+
+        undefined
+    doAction (UpdateBlockchain userIndex) =
+        runUserAction userIndex U.UpdateBlockchain
+
+runUserAction :: WorkMode m => UserIndex -> U.UserCommand -> TestEnv m ()
+runUserAction user command =
+    getUser user >>= flip U.proceedCommand command
+
+getUser :: WorkMode m => UserIndex -> TestEnv m U.RSCoinUserState
+getUser Nothing =
+    view $ buser . state
+getUser (fromJust -> getNonNegative -> index) =
+    fmap fromJust . preview $ users . to cycle . ix index . state
+
 instance Arbitrary SomeAction where
     arbitrary = oneof [ SomeAction <$> (arbitrary :: Gen EmptyAction) -- I am not sure does this makes sense when we have WaitAction
                       , SomeAction <$> (arbitrary :: Gen WaitAction)
+                      , SomeAction <$> (arbitrary :: Gen UserAction)
                       ]
+
+-- TODO: maybe we should create also actions StartMintette, AddMintette, in terms of actions
 
 main :: IO ()
 main = return ()

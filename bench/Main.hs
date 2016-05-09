aa221cc334bfacb8630da59bbcaea1d07b1399e3
ChShersh <kovanikov@gmail.com>
2016-05-10 01:12:02 +0300

    Create bank key in .bench-local folder
    Add simple User thread

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Main where
 
-import           Control.Monad.Trans (liftIO)
-import           Data.Acid           (update)
+import           Control.Monad.Trans (MonadIO, liftIO)
+import           Data.Acid           (update, createCheckpoint)
 import           Data.Text           as T
 
 import qualified RSCoin.Bank         as B
 import           RSCoin.Core         (Mintette (Mintette), Severity (Debug),
-                                      constructPublicKey, defaultSecretKeyPath,
+                                      constructPublicKey, defaultAccountsNumber,
                                       initLogging, logWarning, readPublicKey,
                                       readSecretKey)
 import qualified RSCoin.Mintette     as M
+import qualified RSCoin.User.AcidState as A
+import RSCoin.User.Commands (UserCommand (..), proceedCommand)
+import qualified RSCoin.User.Wallet    as W
 
-import           RSCoin.Test         (MsgPackRpc, bracket', runRealMode)
+import           RSCoin.Timed         (MsgPackRpc, bracket', runRealMode, fork)
 
 import           Control.Concurrent  (forkIO, threadDelay)
 import           Control.Exception   (bracket_)
@@ -21,34 +27,33 @@ import           Control.Monad.Extra (whenM)
 import           System.Directory    (createDirectoryIfMissing,
                                       doesDirectoryExist,
                                       removeDirectoryRecursive)
-import           System.FilePath     ((</>), takeDirectory)
+import           System.FilePath     ((</>))
 
-homeKeyDirectory :: IO FilePath
-homeKeyDirectory = takeDirectory <$> defaultSecretKeyPath
+import           Control.Monad.Catch   (MonadCatch, catch, throwM)
 
 tempBenchDirectory :: FilePath
 tempBenchDirectory = ".bench-local"
 
+bankKeyFilePath :: FilePath
+bankKeyFilePath = tempBenchDirectory </> "rscoin-key"
+
 bankLocalDirectory :: FilePath
 bankLocalDirectory = tempBenchDirectory </> "bank-db"
+
+walletLocalPath :: FilePath
+walletLocalPath = tempBenchDirectory </> "wallet-db"
+
+defaultBankKey :: String
+defaultBankKey = "SecKey \"448d85e1261c2ce919bdbdf1b3830653e91380f4f22ef6d5b0edfb6537dd0772\""
 
 setupBench :: IO ()
 setupBench = do
     createDirectoryIfMissing False tempBenchDirectory
-    
-    createDirectoryIfMissing False =<< homeKeyDirectory
-    skPath <- defaultSecretKeyPath
-    writeFile skPath "SecKey \"448d85e1261c2ce919bdbdf1b3830653e91380f4f22ef6d5b0edfb6537dd0772\""
+    writeFile bankKeyFilePath defaultBankKey
 
 cleanupBench :: IO ()
-cleanupBench = do
-    removeDirectoryRecursiveIfExists tempBenchDirectory
-    removeDirectoryRecursiveIfExists =<< homeKeyDirectory
-  where
-    removeDirectoryRecursiveIfExists :: FilePath -> IO ()
-    removeDirectoryRecursiveIfExists directoryName = 
-        whenM (doesDirectoryExist directoryName) (removeDirectoryRecursive directoryName)
-
+cleanupBench = whenM (doesDirectoryExist tempBenchDirectory) (removeDirectoryRecursive tempBenchDirectory)
+        
 bankBracket :: (B.State -> MsgPackRpc ()) -> IO ()
 bankBracket bankStateFun = 
     runRealMode $ bracket'
@@ -67,24 +72,62 @@ addMintette = bankBracket $ \st -> liftIO $ do
         logWarning "Failed to parse public key, trying to interpret as a filepath to key"
         readPublicKey $ T.unpack pk
 
+bankThread :: IO ()
+bankThread = bankBracket $ \st -> do
+    secretKey <- liftIO $ readSecretKey bankKeyFilePath
+    fork $ B.runWorker secretKey st
+    B.serve st
+
 mintetteThread :: IO ()
 mintetteThread = do
     initLogging Debug
-    skPath <- defaultSecretKeyPath
-    sk     <- readSecretKey skPath
-    runRealMode $ M.serve 1234 (tempBenchDirectory </> "mintette-db") sk
+    secretKey <- readSecretKey bankKeyFilePath
+    runRealMode $
+        bracket' 
+            (liftIO $ M.openState $ tempBenchDirectory </> "mintette-db") 
+            (liftIO . M.closeState) $
+            \st -> do
+                secretKey <- liftIO $ readSecretKey bankKeyFilePath
+                fork $ M.runWorker secretKey st
+                M.serve 1234 st secretKey
 
-bankThread :: IO ()
-bankThread = bankBracket $ \st -> do
-    skPath <- liftIO $ defaultSecretKeyPath
-    sk     <- liftIO $ readSecretKey skPath
-    B.runWorker sk st
-    B.serve st
+userThread :: IO ()
+userThread = runRealMode $ bracket'
+    (liftIO $ A.openState walletLocalPath)
+    (\st -> liftIO $ do
+        createCheckpoint st
+        A.closeState st)
+    (\st -> do 
+        handleUninitialized
+            (proceedCommand st UpdateBlockchain)
+            (A.initState
+                st
+                defaultAccountsNumber
+                Nothing
+                {-(Just bankKeyFilePath)-}))
+
+  where
+    handleUninitialized :: (MonadIO m, MonadCatch m) => m () -> m () -> m ()
+    handleUninitialized action initialize =
+        action `catch` handler initialize action
+    handler i a W.NotInitialized =
+        liftIO (putStrLn "Initializing storage..") >> i >> a
+    handler _ _ e = throwM e
 
 main :: IO ()
 main = bracket_ setupBench cleanupBench $ do
     initLogging Debug
+
     addMintette
-    forkIO mintetteThread
-    forkIO bankThread
-    threadDelay (10^7)
+
+    _ <- forkIO bankThread
+    putStrLn ">>> Starting bank"
+    threadDelay (3^7)
+
+    _ <- forkIO mintetteThread
+    putStrLn ">>> Starting mintette"
+    threadDelay (5^7)
+    
+    _ <- forkIO userThread
+    putStrLn ">>> Starting user"
+    threadDelay (5^7)

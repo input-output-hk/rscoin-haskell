6413bba6aba482c7fb480e0979045f35d921d5b5
ChShersh <kovanikov@gmail.com>
2016-05-15 15:33:35 +0300

    Add some basic transactions from users to each other

--- bench/Bench/RSCoin/UserLogic.hs
+++ bench/Bench/RSCoin/UserLogic.hs
@@ -1,5 +1,6 @@
 module Bench.RSCoin.UserLogic
-        ( initializeBank
+        ( benchUserTransactions
+        , initializeBank
         , initializeUser
         , userThread
         ) where
@@ -16,24 +17,35 @@ import           RSCoin.Core                (Coin (..))
 import qualified RSCoin.User.AcidState      as A
 import           RSCoin.User.Commands       (UserCommand (UpdateBlockchain),
                                              proceedCommand)
-import           RSCoin.User.Operations     (formTransaction)
+import           RSCoin.User.Operations     (getAmount, formTransaction)
 import qualified RSCoin.User.Wallet         as W
 
 import           RSCoin.Timed               (MsgPackRpc, bracket', runRealMode)
 
 import           System.FilePath            ((</>))
 
+userThread :: FilePath -> (A.RSCoinUserState -> MsgPackRpc a) -> Int64 -> IO a
+userThread benchDir userAction userId = runRealMode $ bracket'
+    (liftIO $ A.openState $ benchDir </> dbFormatPath "wallet-db" userId)
+    (\userState -> liftIO $ do
+        createCheckpoint userState
+        A.closeState userState
+    )
+    userAction
+
 queryMyAddress :: A.RSCoinUserState -> MsgPackRpc W.UserAddress
 queryMyAddress userState = do
     allAddresess <- liftIO $ query userState A.GetAllAddresses
     return $ head allAddresess
 
+-- | Create user with 1 address and return it.
 initializeUser :: A.RSCoinUserState -> MsgPackRpc W.UserAddress
 initializeUser userState = do
     let userAddressesNumber = 1
     A.initState userState userAddressesNumber Nothing
     queryMyAddress userState
 
+-- | Create user in `bankMode` and send 1000 coins to every user from list.
 initializeBank :: FilePath -> [W.UserAddress] -> A.RSCoinUserState -> MsgPackRpc ()
 initializeBank bankKeyFilePath userAddresses bankUserState = do
     let additionalBankAddreses = 0
@@ -46,11 +58,21 @@ initializeBank bankKeyFilePath userAddresses bankUserState = do
         proceedCommand bankUserState UpdateBlockchain
         formTransaction bankUserState inputMoneyInfo (W.toAddress userAddr) outputMoney
 
-userThread :: FilePath -> (A.RSCoinUserState -> MsgPackRpc a) -> Int64 -> IO a
-userThread benchDir userAction userId = runRealMode $ bracket'
-    (liftIO $ A.openState $ benchDir </> dbFormatPath "wallet-db" userId)
-    (\userState -> liftIO $ do
-        createCheckpoint userState
-        A.closeState userState
-    )
-    userAction
+-- | Start user with provided addresses of other users and do 1000 transactions.
+benchUserTransactions :: [W.UserAddress] -> A.RSCoinUserState -> MsgPackRpc ()
+benchUserTransactions allAddresses userState = do
+    myAddress         <- queryMyAddress userState
+    liftIO $ putStrLn $ "Real addr: " ++ show myAddress
+    myAmount <- getAmount userState myAddress
+    liftIO $ putStrLn $ "My amount: " ++ show myAmount
+
+    let otherAddresses = filter (/= myAddress) allAddresses
+    let numberOfUsers  = length otherAddresses
+    let outputNumber   = 1
+    let outputMoney    = Coin outputNumber
+    let inputMoneyInfo = [(1, outputNumber)]
+    forM_ [0..3] $ \i -> do
+        liftIO $ putStrLn $ "Iter: " ++ show i
+        let userAddrToSend = otherAddresses !! (i `mod` numberOfUsers)
+        proceedCommand userState UpdateBlockchain
+        formTransaction userState inputMoneyInfo (W.toAddress userAddrToSend) outputMoney
--- bench/Main.hs
+++ bench/Main.hs
@@ -3,18 +3,20 @@ module Main where
 import           Bench.RSCoin.FilePathUtils (defaultBankKey, tempBenchDirectory)
 import           Bench.RSCoin.InfraThreads  (addMintette, bankThread,
                                              mintetteThread)
-import           Bench.RSCoin.UserLogic     (initializeBank, initializeUser,
-                                             userThread)
+import           Bench.RSCoin.UserLogic     (benchUserTransactions, initializeBank,
+                                             initializeUser, userThread)
 
 import           RSCoin.Core                (Severity (Debug), initLogging,
                                              keyGen)
 
 import           Control.Concurrent         (forkIO, threadDelay)
-import           Control.Concurrent.Async   (async, mapConcurrently, wait)
+import           Control.Concurrent.Async   (async, forConcurrently, wait)
 
 import           System.FilePath            ((</>))
 
 import           System.IO.Temp             (withSystemTempDirectory)
+
+import           Control.Monad              (forM_)
 
 main :: IO ()
 main = withSystemTempDirectory tempBenchDirectory $ \benchDir -> do
@@ -27,10 +29,10 @@ main = withSystemTempDirectory tempBenchDirectory $ \benchDir -> do
     addMintette 1 benchDir pk1
 
     _ <- forkIO $ mintetteThread 1 benchDir sk1
-    threadDelay (5 * 10^6)
+    threadDelay (3 * 10^6)
 
     _ <- forkIO $ bankThread benchDir bankKeyFilePath
-    threadDelay (5 * 10^6)
+    threadDelay (3 * 10^6)
 
     let initUserAction = userThread benchDir initializeUser
     let userIds = [1..2]
@@ -39,4 +41,9 @@ main = withSystemTempDirectory tempBenchDirectory $ \benchDir -> do
     -- give money to all users
     let bankId = 0
     userThread benchDir (initializeBank bankKeyFilePath userAddresses) bankId
-    threadDelay (10 * 10^6)
+    threadDelay (5 * 10^6)
+
+    putStrLn $ "Should be addr: " ++ show (userAddresses !! 0)
+    let benchUserAction = userThread benchDir $ benchUserTransactions userAddresses
+    _ <- forM_ userIds benchUserAction
+    threadDelay (5 * 10^6)

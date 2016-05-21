7cd05eda181192962dc8000f59844199fb693214
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-05-21 17:04:04 +0300

    Add expanding, extra transaction col

--- src/User/GUI/RSCoin/GUI.hs
+++ src/User/GUI/RSCoin/GUI.hs
@@ -3,8 +3,7 @@ module GUI.RSCoin.GUI (startGUI) where
 import           Control.Monad          (void)
 import           Control.Monad.IO.Class (liftIO)
 
-import           Graphics.UI.Gtk        (on)
-import           Graphics.UI.Gtk        (AttrOp ((:=)))
+import           Graphics.UI.Gtk        (AttrOp ((:=)), on)
 import qualified Graphics.UI.Gtk        as G
 
 import           GUI.RSCoin.Glade       (GladeMainWindow (..), importGlade)
@@ -13,6 +12,7 @@ data ModelNode = ModelNode
     { mStatus  :: String
     , mTime    :: String
     , mAddress :: String
+    , mAmount  :: Integer
     }
 
 type Model = G.ListStore ModelNode
@@ -20,15 +20,17 @@ type Model = G.ListStore ModelNode
 createRandomModel :: G.TreeView -> IO Model
 createRandomModel view = do
     model <- G.listStoreNew []
-    appendColumn model "Status" statusSetter
-    appendColumn model "Time" timeSetter
-    appendColumn model "Address" addrSetter
+    appendColumn model True "Status" statusSetter
+    appendColumn model True "Time" timeSetter
+    appendColumn model True "Address" addrSetter
+    appendColumn model True "Amount" amountSetter
     G.treeViewSetModel view model
     return model
   where
-    appendColumn model title attributesSetter = do
+    appendColumn model expand title attributesSetter = do
         column <- G.treeViewColumnNew
         G.treeViewColumnSetTitle column title
+        G.treeViewColumnSetExpand column expand
         renderer <- G.cellRendererTextNew
         G.cellLayoutPackStart column renderer False
         G.cellLayoutSetAttributes column renderer model attributesSetter
@@ -36,17 +38,21 @@ createRandomModel view = do
     statusSetter ModelNode{..} = [G.cellText := mStatus]
     timeSetter ModelNode{..} = [G.cellText := mTime]
     addrSetter ModelNode{..} = [G.cellText := mAddress]
+    amountSetter ModelNode{..} = [G.cellText := showSigned mAmount]
+    showSigned a | a > 0     = "+" ++ show a
+                 | otherwise = show a
 
 addRandomData :: Model -> IO ()
 addRandomData model = mapM_ (G.listStoreAppend model) randomModelData
   where
     randomModelData = do
-        st <- ["confirmed", "sent", "unknown"]
         tm <- ["4:19", "4:20", "12:00"]
+        am <- [123, -3456, 12345, -45323]
+        st <- ["confirmed", "sent", "unknown"]
         addr <- [ "A7FUZi67YbBonrD9TrfhX7wnnFxrIRflbMFOpI+r9dOc"
                 , "G7FuzI67zbBbnrD9trfh27anNf2RiRFLBmfBPi+R9DBC"
                 ]
-        return $ ModelNode st tm addr
+        return $ ModelNode st tm addr am
 
 startGUI :: IO ()
 startGUI = do

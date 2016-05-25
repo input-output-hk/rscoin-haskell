f3bae6da91d7d6205e2346427963d4399105fc01
Ivan Gromakovsky <gie1994@gmail.com>
2016-05-25 16:34:23 +0300

    Change hash implementation to blake2

--- src/RSCoin/Core/Crypto/Hashing.hs
+++ src/RSCoin/Core/Crypto/Hashing.hs
@@ -8,18 +8,19 @@ module RSCoin.Core.Crypto.Hashing
        , hash
        ) where
 
-import qualified Crypto.Hash.SHA256     as SHA256
-import           Data.Binary            (Binary, encode)
-import           Data.ByteString        (ByteString)
-import qualified Data.ByteString.Base64 as B64
-import           Data.Hashable          (Hashable)
-import           Data.MessagePack       (MessagePack)
-import           Data.SafeCopy          (base, deriveSafeCopy)
-import           Data.String            (IsString)
-import           Data.Text.Buildable    (Buildable (build))
-import qualified Data.Text.Format       as F
+import qualified Crypto.Hash.BLAKE2.BLAKE2b as BLAKE2
+import           Data.Binary                (Binary, encode)
+import           Data.ByteString            (ByteString)
+import qualified Data.ByteString.Base64     as B64
+import           Data.ByteString.Lazy       (toStrict)
+import           Data.Hashable              (Hashable)
+import           Data.MessagePack           (MessagePack)
+import           Data.SafeCopy              (base, deriveSafeCopy)
+import           Data.String                (IsString)
+import           Data.Text.Buildable        (Buildable (build))
+import qualified Data.Text.Format           as F
 
--- | Hash is just a base64 encoded ByteString.
+-- | Hash is just a ByteString.
 newtype Hash =
     Hash { getHash :: ByteString }
     deriving (Eq, Show, Binary, Ord, Hashable, MessagePack, IsString)
@@ -27,8 +28,13 @@ newtype Hash =
 $(deriveSafeCopy 0 'base ''Hash)
 
 instance Buildable Hash where
-    build = build . F.Shown . getHash
+    build = build . F.Shown . B64.encode . getHash
+
+blake2b256 :: ByteString -> ByteString
+blake2b256 bs = BLAKE2.finalize l $ BLAKE2.update bs $ BLAKE2.initialize l
+  where
+    l = 256 `div` 8
 
 -- | Hash serializable data.
 hash :: Binary t => t -> Hash
-hash = Hash . B64.encode . SHA256.hashlazy . encode
+hash = Hash . blake2b256 . toStrict . encode

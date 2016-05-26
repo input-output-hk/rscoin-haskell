9d2c963776b40d26585e2cf87c0cd9f8db7b7e55
Arseniy Seroka <ars.seroka@gmail.com>
2016-05-26 16:13:14 +0300

    update to a maro recent version

--- admin/install.sh
+++ admin/install.sh
@@ -14,4 +14,4 @@ sudo sh -c "echo 'trusted-binary-caches = https://cache.nixos.org http://hydra.c
 echo "Registering public keys of caches in /etc/nix/nix.conf"
 sudo sh -c "echo 'binary-cache-public-keys = hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.serokell.io-1:he7AKwJKKiOiy8Sau9sPcso9T/PmlVNxcnNpRgcFsps= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=' >> /etc/nix/nix.conf"
 echo "Installing RSCoin"
-nix-install-package --non-interactive --url https://hydra.serokell.io/build/3/nix/pkg/rscoin-0.1.0.0-x86_64-linux.nixpkg
+nix-install-package --non-interactive --url https://hydra.serokell.io/build/35/nix/pkg/rscoin-0.1.0.0-x86_64-linux.nixpkg
--- admin/runAsMintette.sh
+++ admin/runAsMintette.sh
@@ -6,7 +6,7 @@ test -z $1 && echo "No first argument" && exit
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-stack $NIX_STACK exec -- rscoin-bank --log-severity Debug add-mintette --host 127.0.0.1 --port 311$1 --key $(cat $dir/mintetteKeys/mintette$1/mintette$1.pub)
+rscoin-bank --log-severity Debug add-mintette --host 127.0.0.1 --port 311$1 --key $(cat $dir/mintetteKeys/mintette$1/mintette$1.pub)
 cd $dir/mintetteKeys/mintette$1/ 
-stack $NIX_STACK exec -- rscoin-mintette --log-severity Debug --sk $dir/mintetteKeys/mintette$1/mintette$1.sec -p 311$1
+rscoin-mintette --log-severity Debug --sk $dir/mintetteKeys/mintette$1/mintette$1.sec -p 311$1
 

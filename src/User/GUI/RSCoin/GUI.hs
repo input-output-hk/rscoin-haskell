a49cf132df4753f7bc9dfffd79120f943e2c31ac
akegalj <akegalj@gmail.com>
2016-05-27 21:44:17 +0200

    Fix removed icon

--- src/User/GUI/RSCoin/GUI.hs
+++ src/User/GUI/RSCoin/GUI.hs
@@ -37,7 +37,7 @@ loadIcons = mapM_ loadIcon iconList
                , "resources/icons/people.png"
                , "resources/icons/send.png"
                , "resources/icons/options.png"
---               , "resources/icons/sync.png"
+               , "resources/icons/sync.png"
 --               , "resources/icons/network.png"
 --               , "resources/icons/notification_on.png"
                , "resources/icons/notification_off.png"

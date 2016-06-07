2761517ed63dff6e1208fdd697fcbc8627976020
Ivan Gromakovsky <gie1994@gmail.com>
2016-06-07 15:34:08 +0300

    Add bench version of install.sh

---
+++ bench/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo fallocate -l 512M /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+sudo apt-get update
+sudo aptitude install mc htop git pkg-config libgtk-3-dev
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
+echo 'deb http://download.fpcomplete.com/ubuntu trusty main'|sudo tee /etc/apt/sources.list.d/fpco.list
+sudo apt-get install stack -y
+cd "$HOME"
+git clone https://github.com/serokell/rscoin.git
+cd rscoin
+stack setup
+stack bench rscoin --no-run-benchmarks
+

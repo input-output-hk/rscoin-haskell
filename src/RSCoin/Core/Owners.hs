06e57224f98bfe3624cc5e1f2530630bb120e204
ChShersh <kovanikov@gmail.com>
2016-06-09 03:25:47 +0300

    Add possibility to choose `shardSize` function at compile time

--- src/RSCoin/Core/Owners.hs
+++ src/RSCoin/Core/Owners.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 -- | Canonical implementation of owners function which maps transaction id
 -- into list of mintettes responsible for it.
 
@@ -6,21 +8,15 @@ module RSCoin.Core.Owners
        , isOwner
        ) where
 
-import qualified Data.ByteString        as BS
-import           Data.Hashable          (hash)
-import           Data.List              (nub)
+import qualified Data.ByteString          as BS
+import           Data.Hashable            (hash)
+import           Data.List                (nub)
 
-import           RSCoin.Core.Constants  (shardDivider)
-import           RSCoin.Core.Crypto     (getHash)
-import qualified RSCoin.Core.Crypto     as C
-import           RSCoin.Core.Primitives (TransactionId)
-import           RSCoin.Core.Types      (MintetteId, Mintettes)
-
--- | Defines how many mintettes are responsible for one address (shard
--- size), given the number of mintettes total.
-shardSizeScaled :: Int -> Int
-shardSizeScaled i | i < 1 = error "You can't chose majority from < 1 mintette."
-                  | otherwise = max 2 (i `div` shardDivider)
+import           RSCoin.Core.Crypto       (getHash)
+import qualified RSCoin.Core.Crypto       as C
+import           RSCoin.Core.OwnersConfig (getShardFunction)
+import           RSCoin.Core.Primitives   (TransactionId)
+import           RSCoin.Core.Types        (MintetteId, Mintettes)
 
 -- | Takes list of mintettes which is active and stable over current period
 -- and hash of transaction and returns list of mintettes responsible for it.
@@ -31,7 +27,7 @@ owners mintettes h =
     take shardSize $ nub $ map (\i -> hash i `mod` l) $ iterate hashProduce h
   where
     l = length mintettes
-    shardSize = shardSizeScaled l
+    shardSize = $getShardFunction l
     hashProduce i = C.hash $ getHash i `BS.append` getHash h
 
 -- | This function checks whether given mintette is owner of given transaction.
---
+++ src/RSCoin/Core/OwnersConfig.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- Functions that work with compile-time configuration.
+
+module RSCoin.Core.OwnersConfig
+    ( getShardFunction
+    ) where
+
+import           System.Environment    (getEnvironment)
+
+import           Language.Haskell.TH   (Exp, Q, runIO)
+
+import           RSCoin.Core.Constants (shardDivider)
+
+-- | Environment option to use during compilation.
+shardSizeOption :: String  -- using String here because of 'getEnvironment'
+shardSizeOption = "SHARD_SIZE"
+
+-- | Defines how many mintettes are responsible for one address (shard
+-- size), given the number of mintettes total.
+shardSizeScaled :: Int -> Int
+shardSizeScaled i
+    | i < 1     = error "You can't chose majority from < 1 mintette."
+    | otherwise = max 2 (i `div` shardDivider)
+
+-- | Like 'shardSizeScaled' but always return given size.
+constShardSize :: Int -> Int -> Int
+constShardSize fixedSize i
+    | i < 1     = error "You can't chose majority from < 1 mintette."
+    | otherwise = min fixedSize i
+
+-- | Looks up a compile-time environment variable.
+lookupCompileEnv :: String -> Q (Maybe String)
+lookupCompileEnv key = lookup key <$> runIO getEnvironment
+
+-- | Returns proper function whitch depends whether SHARD_SIZE
+-- environment option was passed or not.
+getShardFunction :: Q Exp
+getShardFunction = do
+    mShardSize <- lookupCompileEnv shardSizeOption
+    case mShardSize of
+         Just _  -> [| const $ constShardSize 3 |]
+         Nothing -> [| shardSizeScaled          |]

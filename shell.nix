cb0dccb3eab186f2bf70d68c3951f67baf949cf7
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-06-13 17:46:19 +0300

    Simplify shell.nix file

--- shell.nix
+++ shell.nix
@@ -3,51 +3,20 @@
 let
 
   inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, acid-state, aeson, async, base
-      , base64-bytestring, binary, bytestring, cereal, conduit-extra
-      , containers, cryptohash, data-default, directory, ed25519, either
-      , exceptions, file-embed, filepath, hashable, hslogger, hspec, lens
-      , monad-control, monad-loops, MonadRandom, msgpack, msgpack-aeson
-      , msgpack-rpc, mtl, optparse-applicative, pqueue, QuickCheck
-      , random, safe, safecopy, stdenv, stm, text
-      , text-format, time, time-units, transformers, transformers-base
-      , tuple, unordered-containers, vector
-      , pkgconfig, cairo, gtk3
-      }:
+  dependencies = 
+    with nixpkgs; 
+    [zlib zlib.out git openssh autoreconfHook pkgconfig glib cairo pango gtk3];
+  f = { mkDerivation, stdenv, base, zlib }:
       mkDerivation {
         pname = "rscoin";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
-        libraryHaskellDepends = [
-          acid-state aeson base base64-bytestring binary bytestring cereal
-          conduit-extra containers cryptohash data-default directory ed25519
-          either exceptions file-embed filepath hashable hslogger lens
-          monad-control monad-loops MonadRandom msgpack msgpack-aeson
-          msgpack-rpc mtl pqueue QuickCheck random safe safecopy
-           stm text text-format time time-units transformers
-          transformers-base tuple unordered-containers vector
-        ];
-        executableHaskellDepends = [
-          acid-state aeson base base64-bytestring binary bytestring cereal
-          conduit-extra containers cryptohash data-default directory ed25519
-          exceptions filepath hashable hslogger hspec lens monad-control
-          monad-loops MonadRandom msgpack msgpack-aeson msgpack-rpc mtl
-          optparse-applicative pqueue QuickCheck random safe safecopy
-           stm text text-format time time-units transformers
-          transformers-base tuple unordered-containers vector
-        ];
-        testHaskellDepends = [
-          acid-state async base bytestring containers data-default exceptions
-          hspec lens MonadRandom msgpack msgpack-rpc mtl QuickCheck random
-          safe safecopy  stm text time-units transformers tuple
-          vector
-        ];
-        libraryPkgconfigDepends =
-          (with nixpkgs; [zlib git openssh autoreconfHook]) ++
-          [pkgconfig cairo gtk3];
+        libraryHaskellDepends = dependencies;
+        executableHaskellDepends = dependencies;
+        testHaskellDepends = dependencies;
+        libraryPkgconfigDepends = dependencies;
         license = stdenv.lib.licenses.gpl3;
       };
 

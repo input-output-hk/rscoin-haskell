6f8becad6c34d30d8f78b7bfabdbf55ac0761d5a
Dmitry Kovanikov <kovanikov@gmail.com>
2016-06-17 21:06:55 +0300

    Support start of single user with custom wallet

--- bench/Remote/SingleUser.hs
+++ bench/Remote/SingleUser.hs
@@ -4,7 +4,9 @@
 
 import           Data.ByteString            (ByteString)
 import           Data.Maybe                 (fromMaybe)
+import           Data.Optional              (Optional (Specific), empty)
 import           Formatting                 (build, sformat, (%))
+
 -- workaround to make stylish-haskell work :(
 import           Options.Generic
 import           System.IO.Temp             (withSystemTempDirectory)
@@ -13,14 +15,15 @@ import           RSCoin.Core                (Severity (..), initLogging)
 
 import           Bench.RSCoin.FilePathUtils (tempBenchDirectory)
 import           Bench.RSCoin.Logging       (initBenchLogger, logInfo)
-import           Bench.RSCoin.UserLogic     (runSingleUser, userThread)
+import           Bench.RSCoin.UserLogic     (runSingleUser, userThreadWithPath)
 import           Bench.RSCoin.Util          (ElapsedTime, measureTime_)
 
 data BenchOptions = BenchOptions
-    { severity      :: Maybe Severity <?> "severity for global logger"
+    { bank          :: ByteString     <?> "bank host"
+    , severity      :: Maybe Severity <?> "severity for global logger"
     , benchSeverity :: Maybe Severity <?> "severity for bench logger"
-    , bank          :: ByteString     <?> "bank host"
     , transactions  :: Maybe Word     <?> "number of transactions"
+    , walledDb      :: Maybe FilePath <?> "path to wallet (assuming it has enough money)"
     } deriving (Generic, Show)
 
 instance ParseField  Word
@@ -29,20 +32,27 @@ instance ParseFields Severity
 instance ParseRecord Severity
 instance ParseRecord BenchOptions
 
-run :: Word -> ByteString -> FilePath -> IO ElapsedTime
-run txNum bankHost benchDir =
-    measureTime_ $ userThread bankHost benchDir (const $ runSingleUser txNum) 0
+run :: Word -> ByteString -> FilePath -> Optional FilePath -> IO ElapsedTime
+run txNum bankHost benchDir optWalletPath
+    = measureTime_ $
+      userThreadWithPath bankHost
+                         benchDir
+                         (const $ runSingleUser txNum)
+                         0
+                         optWalletPath
 
 main :: IO ()
 main = do
-    BenchOptions{..} <- getRecord "rscoin-bench-single-user"
+    BenchOptions{..}  <- getRecord "rscoin-bench-single-user"
+
+    let bankHost       = unHelpful bank
     let globalSeverity = fromMaybe Error $ unHelpful severity
-        bSeverity = fromMaybe Info $ unHelpful benchSeverity
-        bankHost = unHelpful bank
-        txNum = fromMaybe 100 $ unHelpful transactions
-    withSystemTempDirectory tempBenchDirectory $
-        \benchDir ->
-             do initLogging globalSeverity
-                initBenchLogger bSeverity
-                logInfo . sformat ("Elapsed time: " % build) =<<
-                    run txNum bankHost benchDir
+    let bSeverity      = fromMaybe Info  $ unHelpful benchSeverity
+    let txNum          = fromMaybe 100   $ unHelpful transactions
+    let walletPath     = maybe empty Specific $ unHelpful walledDb
+
+    withSystemTempDirectory tempBenchDirectory $ \benchDir -> do
+        initLogging globalSeverity
+        initBenchLogger bSeverity
+        logInfo . sformat ("Elapsed time: " % build) =<<
+            run txNum bankHost benchDir walletPath

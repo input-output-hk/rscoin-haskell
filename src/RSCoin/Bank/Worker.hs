c201dd388db08bdd8e756c353a1e80b0ac52cf2b
Ivan Gromakovsky <gie1994@gmail.com>
2016-06-18 21:54:35 +0300

    Bank: print time spent on finishing period

--- src/RSCoin/Bank/Worker.hs
+++ src/RSCoin/Bank/Worker.hs
@@ -12,48 +12,62 @@ module RSCoin.Bank.Worker
 
 import           Control.Exception        (SomeException)
 import           Control.Monad.Catch      (catch)
-import           Control.Monad.Trans      (liftIO)
+import           Control.Monad.Trans      (MonadIO (liftIO))
 import           Data.Acid                (createCheckpoint)
 import           Data.Acid.Advanced       (query', update')
 import           Data.IORef               (modifyIORef, newIORef, readIORef)
 import           Data.Monoid              ((<>))
+import           Data.Text                (Text)
 import           Data.Time.Units          (TimeUnit)
+import           Formatting               (build, sformat, (%))
 
+import           Serokell.Util.Bench      (measureTime_)
 import           Serokell.Util.Exceptions ()
 import           Serokell.Util.Text       (formatSingle')
 
-import           RSCoin.Core              (Mintettes, PeriodId, PeriodResult,
-                                           SecretKey, announceNewPeriod,
-                                           bankLoggerName, formatNewPeriodData,
-                                           logDebug, logError, logInfo,
-                                           logWarning, defaultPeriodDelta,
-                                           sendPeriodFinished)
-
 import           RSCoin.Bank.AcidState    (GetMintettes (..), GetPeriodId (..),
                                            StartNewPeriod (..), State)
+import           RSCoin.Core              (Mintettes, PeriodId, PeriodResult,
+                                           defaultPeriodDelta,
+                                           formatNewPeriodData,
+                                           sendPeriodFinished)
+import qualified RSCoin.Core              as C
 import           RSCoin.Timed             (WorkMode, minute, repeatForever, tu)
 
+logDebug :: MonadIO m => Text -> m ()
+logDebug = C.logDebug C.bankLoggerName
+
+logInfo :: MonadIO m => Text -> m ()
+logInfo = C.logInfo C.bankLoggerName
+
+logWarning :: MonadIO m => Text -> m ()
+logWarning = C.logWarning C.bankLoggerName
+
+logError :: MonadIO m => Text -> m ()
+logError = C.logError C.bankLoggerName
+
 -- | Start worker which runs appropriate action when a period finishes
-runWorker :: WorkMode m => SecretKey -> State -> m ()
+runWorker :: WorkMode m => C.SecretKey -> State -> m ()
 runWorker = runWorkerWithPeriod defaultPeriodDelta
 
 -- | Start worker with provided period. Used in benchmarks. Also see 'runWorker'.
-runWorkerWithPeriod :: (TimeUnit t, WorkMode m) => t -> SecretKey -> State -> m ()
-runWorkerWithPeriod periodDelta sk st = repeatForever (tu periodDelta) handler $
-    onPeriodFinished sk st
+runWorkerWithPeriod :: (TimeUnit t, WorkMode m) => t -> C.SecretKey -> State -> m ()
+runWorkerWithPeriod periodDelta sk st = repeatForever (tu periodDelta) handler $ do
+    t <- measureTime_ $ onPeriodFinished sk st
+    logInfo $ sformat ("Finishing period took " % build) t
   where
     handler e = do
-        logError bankLoggerName $
+        logError $
             formatSingle'
                 "Error was caught by worker, restarting in 1 minute: {}"
                 e
         return $ minute 1
 
-onPeriodFinished :: WorkMode m => SecretKey -> State -> m ()
+onPeriodFinished :: WorkMode m => C.SecretKey -> State -> m ()
 onPeriodFinished sk st = do
     mintettes <- query' st GetMintettes
     pId <- query' st GetPeriodId
-    logInfo bankLoggerName $ formatSingle' "Period {} has just finished!" pId
+    logInfo $ formatSingle' "Period {} has just finished!" pId
     -- Mintettes list is empty before the first period, so we'll simply
     -- get [] here in this case (and it's fine).
     periodResults <- getPeriodResults mintettes pId
@@ -61,27 +75,27 @@ onPeriodFinished sk st = do
     liftIO $ createCheckpoint st
     newMintettes <- query' st GetMintettes
     if null newMintettes
-        then logWarning bankLoggerName "New mintettes list is empty!"
+        then logWarning "New mintettes list is empty!"
         else do
             mapM_
                 (\(m,mId) ->
-                      announceNewPeriod m (newPeriodData !! mId) `catch`
+                      C.announceNewPeriod m (newPeriodData !! mId) `catch`
                       handlerAnnouncePeriod)
                 (zip newMintettes [0 ..])
-            logInfo bankLoggerName $
+            logInfo $
                 formatSingle'
                     ("Announced new period with this NewPeriodData " <>
                      "(payload is Nothing -- omitted (only in Debug)):\n{}")
                     (formatNewPeriodData False $ head newPeriodData)
-            logDebug bankLoggerName $
+            logDebug $
                 formatSingle'
-                   "Announced new period, sent these newPeriodData's:\n{}"
-                   newPeriodData
+                    "Announced new period, sent these newPeriodData's:\n{}"
+                    newPeriodData
   where
     -- TODO: catch appropriate exception according to protocol
     -- implementation (here and below)
     handlerAnnouncePeriod (e :: SomeException) =
-        logWarning bankLoggerName $
+        logWarning $
         formatSingle' "Error occurred in communicating with mintette {}" e
 
 getPeriodResults :: WorkMode m => Mintettes -> PeriodId -> m [Maybe PeriodResult]
@@ -91,10 +105,13 @@ getPeriodResults mts pId = do
     liftIO $ reverse <$> readIORef res
   where
     f res mintette =
-        (sendPeriodFinished mintette pId
-            >>= liftIO . modifyIORef res . (:) . Just)
-                `catch` handler res
-    handler res (e :: SomeException) = liftIO $ do
-        logWarning bankLoggerName $
-            formatSingle' "Error occurred in communicating with mintette {}" e
-        modifyIORef res (Nothing :)
+        (sendPeriodFinished mintette pId >>=
+         liftIO . modifyIORef res . (:) . Just) `catch`
+        handler res
+    handler res (e :: SomeException) =
+        liftIO $
+        do logWarning $
+               formatSingle'
+                   "Error occurred in communicating with mintette {}"
+                   e
+           modifyIORef res (Nothing :)

b0fc7462e410812238d67681862a7f8d20c26eb2
Dmitry Kovanikov <kovanikov@gmail.com>
2016-06-19 19:50:10 +0300

    Creating directory from turtle makes me sad :(

--- bench/Remote/Wrapper.hs
+++ bench/Remote/Wrapper.hs
@@ -437,7 +437,7 @@ writeUserTPSInfo
     -> Double
     -> IO ()
 writeUserTPSInfo RemoteConfig{..} udm userDatas totalTPS = do
-    () <$ T.proc "sh" [mkStatsDirCommand] mempty
+    () <$ T.proc "bash" ["-c", "'mkdir -p \"$HOME\"/rscoin-stats'"] mempty
     let dateFileName    = mconcat [statsDir, "/", statsId, ".stats"]
     let mintettesNum    = length rcMintettes
     let usersNum        = length userDatas

c84a49ae7ebd043b9eea6721a7c5bb956a513a72
Ivan Gromakovsky <gie1994@gmail.com>
2016-06-20 23:11:09 +0300

    Fix bench version of install.sh

--- bench/install.sh
+++ bench/install.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo aptitude install mc htop git pkg-config libgtk-3-dev
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
 echo 'deb http://download.fpcomplete.com/ubuntu trusty main'|sudo tee /etc/apt/sources.list.d/fpco.list
-sudo apt-get install stack -y
+sudo apt-get update && sudo apt-get install stack -y
 cd "$HOME"
 git clone https://github.com/serokell/rscoin.git
 cd rscoin

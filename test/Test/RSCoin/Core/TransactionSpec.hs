6fb3e6321984337f672ea3cf36bbf555913abf03
Ivan Gromakovsky <gie1994@gmail.com>
2016-06-22 12:47:00 +0300

    Implement TransactionValid generation (one bug still remains)

--- test/Test/RSCoin/Core/TransactionSpec.hs
+++ test/Test/RSCoin/Core/TransactionSpec.hs
@@ -7,17 +7,28 @@ module Test.RSCoin.Core.TransactionSpec
        ) where
 
 import           Data.Bifunctor             (first, second)
-import qualified Data.Map.Strict            as M (Map, elems, (!))
+import           Data.List                  (genericLength)
+import qualified Data.Map.Strict            as M (elems, (!))
 import           Test.Hspec                 (Spec, describe)
 import           Test.Hspec.QuickCheck      (prop)
 import           Test.QuickCheck            (Arbitrary (arbitrary), Gen,
-                                             NonEmptyList (..), vector)
+                                             NonEmptyList (..), choose, vector)
 
 import qualified RSCoin.Core                as C
 
 import           Test.RSCoin.Core.Arbitrary ()
 
-newtype TransactionValid = TransactionValid { getTr :: C.Transaction} deriving Show
+newtype TransactionValid = TransactionValid
+    { getTr :: C.Transaction
+    } deriving (Show)
+
+genRationalInRange :: Rational -> Rational -> Gen Rational
+genRationalInRange lo hi =
+    (toRational :: Double -> Rational) <$>
+    choose (fromRational lo, fromRational hi)
+
+genCoinInRange :: C.Color -> Rational -> Rational -> Gen C.Coin
+genCoinInRange col lo hi = C.Coin col <$> genRationalInRange lo hi
 
 instance Arbitrary TransactionValid where
     arbitrary =
@@ -34,31 +45,23 @@ instance Arbitrary TransactionValid where
                        (\(ind,coin) ->
                              (coin, (trid, ind, coin)))
                        inps
-               fun coin = do
-                   v_c <- arbitrary :: Gen Rational
-                   addr <- arbitrary :: Gen C.Address
-                   let cn = C.getCoin coin
-                   return $
-                       ( addr
-                       , if (C.getColor coin) == 0
-                             then v_c `mod` cn
-                             else mod ((C.getCoin $ coins M.! 0) - v_c) cn)
-           coinMap <- mapM fun coins
+               genOutput C.Coin{..} =
+                   (,) <$> arbitrary <*> genCoinInRange getColor 0 getCoin
+           unpaintedOutputsMap <- mapM genOutput coins
            padCols <- arbitrary :: Gen [C.Color]
-           let v = snd $ coinMap M.! 0
-               l = length padCols
+           let v = C.getCoin $ snd $ unpaintedOutputsMap M.! 0
+               l
+                   :: Num a
+                   => a
+               l = genericLength padCols
                helper adr cl cn = (adr, C.Coin cl cn)
            padAddrs <- vector l :: Gen [C.Address]
            return $
                C.Transaction inputs $
-               M.elems coinMap ++
-               if l == 0
+               M.elems unpaintedOutputsMap ++
+               if null padCols
                    then []
-                   else zipWith3
-                            helper
-                            padAddrs
-                            padCols
-                            (repeat (v / fromIntegral l))
+                   else zipWith3 helper padAddrs padCols (repeat (v / l))
 
 spec :: Spec
 spec =

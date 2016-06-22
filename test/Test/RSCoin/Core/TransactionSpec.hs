04b89e74c4c1fa607df5d8d6ceca0f4d7ef66a6e
Ivan Gromakovsky <gie1994@gmail.com>
2016-06-22 12:12:41 +0300

    TransactionSpec: cosmetics
    Apply stylish-haskell, hindent, use standard function.

--- test/Test/RSCoin/Core/TransactionSpec.hs
+++ test/Test/RSCoin/Core/TransactionSpec.hs
@@ -4,41 +4,59 @@ module Test.RSCoin.Core.TransactionSpec
        ( spec
        ) where
 
-import           Test.Hspec                     (Spec, describe)
-import           Test.Hspec.QuickCheck          (prop)
-import           Test.QuickCheck                (Arbitrary (arbitrary), NonNegative (..), NonEmptyList (..), vector, Gen)
-import           RSCoin.Core.Primitives         (Transaction (..), Coin (..))
-import qualified RSCoin.Core as C               (validateSum, Coin (..), Color, Transaction (..))
-import qualified Data.Map.Strict as M           (Map, (!), elems)
+import           Data.Bifunctor             (first)
+import qualified Data.Map.Strict            as M (Map, elems, (!))
+import           Test.Hspec                 (Spec, describe)
+import           Test.Hspec.QuickCheck      (prop)
+import           Test.QuickCheck            (Arbitrary (arbitrary), Gen,
+                                             NonEmptyList (..),
+                                             NonNegative (..), vector)
 
-import           Test.RSCoin.Core.Arbitrary     ()
+import qualified RSCoin.Core                as C
+
+import           Test.RSCoin.Core.Arbitrary ()
 
 newtype TransactionValid = TransactionValid { getTr :: C.Transaction} deriving Show
 
 instance Arbitrary TransactionValid where
-    arbitrary = TransactionValid <$>
-                do trid <- arbitrary :: Gen C.Hash
-                   NonEmpty inps <- arbitrary :: Gen (NonEmptyList (Int,C.Coin))
-                   let (coins,inputs) = fork C.coinsToMap id $
-                                        unzip $
-                                        map (\(ind,coin) -> (coin,(trid,ind,coin))) inps
-                       fork f g (x,y) = (f x, g y)
-                       fun coin = do v_c <- arbitrary :: Gen Rational
-                                     addr <- arbitrary :: Gen C.Address
-                                     let cn = C.getCoin coin
-                                     return $ (addr, if (C.getColor coin) == 0
-                                                         then v_c `mod` cn
-                                                         else mod ((C.getCoin $ coins M.! 0) - v_c) cn)
-                   coinMap <- mapM fun coins
-                   padCols <- arbitrary :: Gen [C.Color]
-                   let v = snd $ coinMap M.! 0
-                       l = length padCols
-                       helper adr cl cn = (adr,C.Coin cl cn)
-                   padAddrs <- vector l :: Gen [C.Address]
-                   return $ C.Transaction inputs $ M.elems coinMap ++
-                       if l == 0
-                            then []
-                            else zipWith3 helper padAddrs padCols (repeat (v / fromIntegral l))
+    arbitrary =
+        TransactionValid <$>
+        do trid <- arbitrary :: Gen C.Hash
+           NonEmpty inps <- arbitrary :: Gen (NonEmptyList (Int, C.Coin))
+           let coins :: C.CoinsMap
+               inputs :: [AddrId]
+               (coins,inputs) =
+                   first C.coinsToMap $
+                   unzip $
+                   map
+                       (\(ind,coin) ->
+                             (coin, (trid, ind, coin)))
+                       inps
+               fun coin = do
+                   v_c <- arbitrary :: Gen Rational
+                   addr <- arbitrary :: Gen C.Address
+                   let cn = C.getCoin coin
+                   return $
+                       ( addr
+                       , if (C.getColor coin) == 0
+                             then v_c `mod` cn
+                             else mod ((C.getCoin $ coins M.! 0) - v_c) cn)
+           coinMap <- mapM fun coins
+           padCols <- arbitrary :: Gen [C.Color]
+           let v = snd $ coinMap M.! 0
+               l = length padCols
+               helper adr cl cn = (adr, C.Coin cl cn)
+           padAddrs <- vector l :: Gen [C.Address]
+           return $
+               C.Transaction inputs $
+               M.elems coinMap ++
+               if l == 0
+                   then []
+                   else zipWith3
+                            helper
+                            padAddrs
+                            padCols
+                            (repeat (v / fromIntegral l))
 
 spec :: Spec
 spec =

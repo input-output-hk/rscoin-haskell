83046242847f89fc233ef78b0abb4ec294f6a3e4
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-06-25 02:00:09 +0100

    Add coinsMapOperation test

--- test/Test/RSCoin/Core/CoinSpec.hs
+++ test/Test/RSCoin/Core/CoinSpec.hs
@@ -9,7 +9,7 @@ module Test.RSCoin.Core.CoinSpec
 import           Test.Hspec                 (Spec, describe)
 import           Test.Hspec.QuickCheck      (prop)
 import           Test.QuickCheck            (NonEmptyList (..))
-import qualified Data.Map.Strict            as M (size)
+import qualified Data.Map.Strict            as M (size, mapWithKey, member, (!))
 
 import qualified RSCoin.Core                as C
 
@@ -24,13 +24,18 @@ spec =
             prop description_sumCoinReturnsSum sumCoinReturnsSum
         describe "coinsToMap" $ do
             prop description_coinsToMapTest coinsToMapTest
+        describe "coinsMapOperation" $ do
+            prop description_coinsMapOperation coinsMapOperation
   where
     description_sumCoinReturnsSum = "given non-empty list of coins with " ++
       "the same color, returns coin with the same color and value equal to " ++
       "sum of values"
     description_coinsToMapTest =
-        "returns true if for a list of arbitrary coins with only one color " ++
-        "coinsToMap will return a map with a single color as its key."
+      "returns true if for a list of arbitrary coins with only one color " ++
+      "coinsToMap will return a map with a single color as its key."
+    description_coinsMapOperation =
+      "returns true if the sum and subtraction of the two inputs maps " ++
+      "resultant from addcoinsMap and subtractCoinsMap is correct"
 
 sameColorCorrect :: C.Coin -> C.Coin -> Bool
 sameColorCorrect c1 c2 = (C.getColor c1 == C.getColor c2) == C.sameColor c1 c2
@@ -52,3 +57,19 @@ coinsToMapTest coins =
     in if null coins'
            then True
            else M.size cMap == 1
+
+coinsMapOperation :: C.CoinsMap -> C.CoinsMap -> Bool
+coinsMapOperation mp1 mp2 =
+    let f = M.mapWithKey (\col (C.Coin _ cn) ->
+                                    C.Coin col cn)
+        (m1,m2) = (f mp1, f mp2)
+        addMap = C.addCoinsMap m1 m2
+        minusMap = C.subtractCoinsMap m1 m2
+        step op col coin = if col `M.member` m2
+                               then coin == (m1 M.! col) `op` (m2 M.! col)
+                               else True
+        stepP = step (+)
+        stepM = step (-)
+    in and $ (and $ M.mapWithKey stepP addMap,
+              and $ M.mapWithKey stepM minusMap)
+        

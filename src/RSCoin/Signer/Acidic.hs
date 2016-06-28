bf7f4bb5a8fa02899f5f350bf7f9e5126bf1ccb2
georgeee <george.agapov@gmail.com>
2016-06-28 16:43:00 +0300

    inroduced basic Signer infrastructure

--- src/RSCoin/Signer/Acidic.hs
+++ src/RSCoin/Signer/Acidic.hs
@@ -5,46 +5,32 @@
 -- main AcidState only in this way extending AcidState is possible
 -- (and it's done in testing framework)
 
-module RSCoin.Mintette.Acidic
+module RSCoin.Signer.Acidic
        ( openState
        , openMemState
        , closeState
-       , GetUtxoPset (..)
-       , PreviousMintetteId (..)
-       , CheckNotDoubleSpent (..)
-       , CommitTx (..)
-       , FinishPeriod (..)
-       , StartPeriod (..)
-       , FinishEpoch (..)
-       , GetBlocks (..)
-       , GetLogs (..)
+       , SignTx (..)
+       , GetSignedTx (..)
        ) where
 
 import           Data.Acid                 (closeAcidState, makeAcidic,
                                             openLocalStateFrom)
 import           Data.Acid.Memory          (openMemoryState)
 
-import           RSCoin.Mintette.AcidState (State)
-import qualified RSCoin.Mintette.AcidState as S
-import qualified RSCoin.Mintette.Storage   as MS
+import           RSCoin.Signer.AcidState (State)
+import qualified RSCoin.Signer.AcidState as S
+import qualified RSCoin.Signer.Storage   as SS
 
 openState :: FilePath -> IO State
-openState fp = openLocalStateFrom fp MS.mkStorage
+openState fp = openLocalStateFrom fp SS.mkStorage
 
 openMemState :: IO State
-openMemState = openMemoryState MS.mkStorage
+openMemState = openMemoryState SS.mkStorage
 
 closeState :: State -> IO ()
 closeState = closeAcidState
 
-$(makeAcidic ''MS.Storage
-             [ 'S.getUtxoPset
-             , 'S.previousMintetteId
-             , 'S.checkNotDoubleSpent
-             , 'S.commitTx
-             , 'S.finishPeriod
-             , 'S.startPeriod
-             , 'S.finishEpoch
-             , 'S.getBlocks
-             , 'S.getLogs
+$(makeAcidic ''SS.Storage
+             [ 'S.signTx
+             , 'S.getSignedTx
              ])
--- src/RSCoin/Signer/Types.hs
+++
@@ -1,9 +0,0 @@
-module RScoin.Signer.Types
-  ( Response(..)
-  , AsyncResponse(..)
-  ) where
-
-import qualified RSCoin.Core as C
-
-data Response = Approved C.Signature | Rejected
-data AsyncResponse = Ready Response | Await

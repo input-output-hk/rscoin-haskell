4e21f170dac77389bdab4526ea1984fea4e7fdfb
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-01 01:26:00 +0300

    Add forgotten module

---
+++ test/Test/RSCoin/Full/Constants.hs
@@ -0,0 +1,26 @@
+-- | Constants in full tests.
+
+module Test.RSCoin.Full.Constants
+       ( userAddressesCount
+       , bankUserAddressesCount
+       , minColor
+       , maxColor
+       ) where
+
+import           RSCoin.Core (Color)
+
+-- | Number of addresses each casual user has in wallet (constant).
+userAddressesCount :: Num a => a
+userAddressesCount = 5
+
+-- | Number of addresses each bank user has in wallet (constant).
+bankUserAddressesCount :: Num a => a
+bankUserAddressesCount = 6
+
+-- | Minimal color used in tests.
+minColor :: Color
+minColor = -2
+
+-- | Maximal color used in tests.
+maxColor :: Color
+maxColor = 5

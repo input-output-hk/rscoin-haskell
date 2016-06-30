6b5ee22d2ef4bca17fc991a69f5556f07457e3fd
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-01 01:12:21 +0300

    Rework valid actions generation
    Goodbye, cool thing :(

--- test/Test/RSCoin/Full/Gen.hs
+++ test/Test/RSCoin/Full/Gen.hs
@@ -12,22 +12,13 @@ module Test.RSCoin.Full.Gen
        ( genValidActions
        ) where
 
-import           Control.Lens                    (Traversal', ix, makeLenses,
-                                                  use, (%=))
 import           Control.Monad                   (replicateM)
-import           Control.Monad.State             (MonadState, StateT,
-                                                  evalStateT)
-import           Control.Monad.Trans             (lift)
 import           Data.Either.Combinators         (mapRight)
-import           Data.List                       (genericIndex, genericLength,
-                                                  genericReplicate)
 import qualified Data.Map                        as M
-import           Data.Maybe                      (catMaybes)
 import           Data.Time.Units                 (fromMicroseconds)
 import           Test.QuickCheck                 (Arbitrary (arbitrary), Gen,
-                                                  NonEmptyList (..), choose,
-                                                  listOf, oneof, sized,
-                                                  sublistOf, suchThat, vectorOf)
+                                                  choose, listOf, shuffle,
+                                                  sized, suchThat, vectorOf)
 
 import qualified RSCoin.Core                     as C
 
@@ -36,8 +27,7 @@ import           Test.RSCoin.Full.Action         (Coloring (Coloring),
                                                   PartToSend (..), PartsToSend,
                                                   SomeAction (SomeAction),
                                                   ToAddress, UserAction (..),
-                                                  UserIndex, WaitAction (..),
-                                                  applyPartsToSend)
+                                                  WaitAction (..))
 import           Test.RSCoin.Full.Constants      (maxColor, minColor)
 import           Test.RSCoin.Full.Context        (MintetteNumber, UserNumber)
 import           Test.RSCoin.Full.Initialization (bankUserAddressesCount,
@@ -91,91 +81,10 @@ genToAddress (fromIntegral -> userNumber) = mapRight fixIndices <$> arbitrary
               usrIdx
               addrIdx)
 
--- | I-th element in this list stores CoinsMap for `i-th` address.
-type BalancesList = [C.CoinsMap]
-
-data AllBalances = AllBalances
-    { _bankBalances  :: BalancesList
-    , _usersBalances :: [BalancesList]
-    } deriving (Show)
-
-$(makeLenses ''AllBalances)
-
-balancesLens :: UserIndex -> Traversal' AllBalances BalancesList
-balancesLens =
-    maybe
-        bankBalances
-        (\i ->
-              usersBalances . ix (fromIntegral i))
-
-zeroBalance :: C.CoinsMap
-zeroBalance = M.empty
-
-genesisBalance :: C.CoinsMap
-genesisBalance = C.coinsToMap [C.genesisValue]
-
-initialBalances :: UserNumber -> AllBalances
-initialBalances userNumber =
-    AllBalances
-    { _bankBalances = genesisBalance :
-      replicate (bankUserAddressesCount - 1) zeroBalance
-    , _usersBalances = genericReplicate userNumber $
-      replicate userAddressesCount zeroBalance
-    }
-
--- It may be impossible if there were transactions to arbitrary
--- address (in this case there may be effectively no coins in system).
-genValidSubmitTransaction :: StateT AllBalances Gen (Maybe UserAction)
-genValidSubmitTransaction = do
-    bb <- (Nothing, ) <$> use bankBalances
-    ub <- zip (fmap Just [0 ..]) <$> use usersBalances
-    genValidSubmitTransactionDo .
-        filter (not . null . C.coinsToList . C.mergeCoinsMaps . snd) $
-        bb : ub
-
-genValidSubmitTransactionDo :: [(UserIndex, BalancesList)]
-                            -> StateT AllBalances Gen (Maybe UserAction)
-genValidSubmitTransactionDo [] = return Nothing
-genValidSubmitTransactionDo solvents =
-    Just <$>
-    do (uIdx,uBalances) <- lift . oneof . map pure $ solvents
-       let nonEmptyAddresses =
-               map fst . filter (not . M.null . snd) . zip [0 ..] $ uBalances
-       indicesToUse <- lift $ nonEmptySublistOf nonEmptyAddresses
-       fromAddresses <-
-           mapM
-               (\i ->
-                     (i, ) <$>
-                     lift (genPartsToSend $ uBalances `genericIndex` i))
-               indicesToUse
-       mapM_ (uncurry $ subtractFromInput uIdx) fromAddresses
-       userNumber <- genericLength <$> use usersBalances
-       dest <- lift $ genToAddress userNumber
-       addToDestination uIdx fromAddresses dest
-       SubmitTransaction uIdx (NonEmpty fromAddresses) dest <$> lift arbitrary
-  where
-    nonEmptySublistOf :: [a] -> Gen [a]
-    nonEmptySublistOf xs = sublistOf xs `suchThat` (not . null)
-    subtractFromInput usrIndex usrAddrIndex parts =
-        balancesLens usrIndex . ix (fromIntegral usrAddrIndex) %=
-        decreaseBalance parts
-    decreaseBalance :: PartsToSend -> C.CoinsMap -> C.CoinsMap
-    decreaseBalance parts coinsMap =
-        C.subtractCoinsMap coinsMap $ applyPartsToSend parts coinsMap
-    addToDestination
-        :: MonadState AllBalances m
-        => UserIndex -> [(Word, PartsToSend)] -> ToAddress -> m ()
-    addToDestination _ _ (Left _) = return ()
-    addToDestination srcUsrIdx inputs (Right (dstUsrIdx,dstUsrAddrIdx)) = do
-        balances <- use $ balancesLens srcUsrIdx
-        let step (addrIdx,parts) =
-                applyPartsToSend parts $ balances `genericIndex` addrIdx
-            dstLens = balancesLens dstUsrIdx . ix (fromIntegral dstUsrAddrIdx)
-            maps = map step inputs
-        mapM_
-            (\summand ->
-                  dstLens %= C.addCoinsMap summand)
-            maps
+genSubmitTransaction :: UserNumber -> Gen UserAction
+genSubmitTransaction un =
+    SubmitTransaction <$> arbitrary <*> arbitrary <*> genToAddress un <*>
+    arbitrary
 
 genUpdateBlockchain :: Gen UserAction
 genUpdateBlockchain = UpdateBlockchain <$> arbitrary
@@ -191,11 +100,10 @@ genValidActions userNumber = do
     actions <- mapM genWaitAction userActions
     return (map SomeAction actions)
   where
-    genUserActions s =
-        let updates = s `div` 10
-            transactions = s - updates
-        in (++) <$> replicateM updates genUpdateBlockchain <*>
-           (catMaybes <$>
-            evalStateT
-                (replicateM transactions genValidSubmitTransaction)
-                (initialBalances userNumber))
+    genUserActions s = do
+        let updatesNum = s `div` 10
+            transactionsNum = s - updatesNum
+        updates <- vectorOf updatesNum genUpdateBlockchain
+        transactions <-
+            vectorOf transactionsNum $ genSubmitTransaction userNumber
+        shuffle $ updates ++ transactions

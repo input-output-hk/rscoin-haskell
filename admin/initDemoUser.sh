9ddd2093018cacbe1f6a249bcf1f09405f761bcb
georgeee <george.agapov@gmail.com>
2016-07-03 01:58:59 +0300

    Fix demo9

--- admin/initDemoUser.sh
+++ admin/initDemoUser.sh
@@ -1,3 +1,3 @@
 mkdir wallet-db-user
-rscoin-user -- --wallet-path wallet-db-user update
-rscoin-user -- --wallet-path wallet-db-user list
+rscoin-user --wallet-path wallet-db-user update
+rscoin-user --wallet-path wallet-db-user list
--- admin/watchBlockchainUpdates.sh
+++ admin/watchBlockchainUpdates.sh
@@ -1,5 +1,5 @@
 while true; do
-  rscoin-user -- --bank-mode update
-  rscoin-user -- --wallet-path wallet-db-user update
+  rscoin-user --bank-mode update
+  rscoin-user --wallet-path wallet-db-user update
   sleep 10
 done

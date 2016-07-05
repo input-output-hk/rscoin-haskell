b6641772e2667315e6d855731cfb4cbbc2044167
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-07-05 22:39:21 +0300

    Restore aliases.sh

---
+++ admin/aliases.sh
@@ -0,0 +1,3 @@
+alias rscoin-mintette="stack $NIX_STACK exec -- rscoin-mintette --log-severity Debug"
+alias rscoin-bank="stack $NIX_STACK exec -- rscoin-bank --log-severity Debug"
+alias rscoin-user="stack $NIX_STACK exec -- rscoin-user"

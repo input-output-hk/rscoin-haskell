7ae82b9f9c44f7bc17d61c9dbf13f5c8df797b79
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-07-06 14:03:49 +0300

    Add signer to aliases

--- admin/aliases.sh
+++ admin/aliases.sh
@@ -1,3 +1,4 @@
 alias rscoin-mintette="stack $NIX_STACK exec -- rscoin-mintette --log-severity Debug"
 alias rscoin-bank="stack $NIX_STACK exec -- rscoin-bank --log-severity Debug"
 alias rscoin-user="stack $NIX_STACK exec -- rscoin-user"
+alias rscoin-signer="stack $NIX_STACK exec -- rscoin-signer --log-severity Debug"

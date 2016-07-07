3938b98004043b8fe745384968979f6b8871724d
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-07 18:02:46 +0300

    Define ExplorerOptions

--- src/Explorer/ExplorerOptions.hs
+++ src/Explorer/ExplorerOptions.hs
@@ -2,6 +2,52 @@
 
 module ExplorerOptions
        ( Options (..)
+       , getOptions
        ) where
 
+import           Options.Applicative    (Parser, auto, execParser, fullDesc,
+                                         help, helper, info, long, metavar,
+                                         option, progDesc, short, showDefault,
+                                         value, (<>))
+import           Serokell.Util.OptParse (strOption)
+
+import           RSCoin.Core            (Severity (Error), defaultPort,
+                                         defaultSecretKeyPath)
+
 data Options = Options
+    { cloPort          :: Int
+    , cloPath          :: FilePath
+    , cloSecretKeyPath :: FilePath
+    , cloLogSeverity   :: Severity
+    }
+
+optionsParser :: FilePath -> Parser Options
+optionsParser defaultSKPath =
+    Options <$>
+    option
+        auto
+        (mconcat [short 'p', long "port", value defaultPort, showDefault]) <*>
+    strOption
+        (mconcat
+             [ long "path"
+             , value "explorer-db"
+             , showDefault
+             , help "Path to database"]) <*>
+    strOption
+        (mconcat
+             [long "sk", value defaultSKPath, metavar "FILEPATH", showDefault]) <*>
+    option
+        auto
+        (mconcat
+             [ long "log-severity"
+             , value Error
+             , showDefault
+             , help "Logging severity"])
+
+getOptions :: IO Options
+getOptions = do
+    defaultSKPath <- defaultSecretKeyPath
+    execParser $
+        info
+            (helper <*> optionsParser defaultSKPath)
+            (fullDesc <> progDesc "RSCoin Block Explorer")
--- src/Explorer/Main.hs
+++ src/Explorer/Main.hs
@@ -1,2 +1,11 @@
+import qualified RSCoin.Core     as C
+
+import           ExplorerOptions (Options (..), getOptions)
+
 main :: IO ()
-main = putStrLn "Blockchain = DEAD"
+main = do
+    Options {..} <- getOptions
+    C.initLogging cloLogSeverity
+    sk <- C.readSecretKey cloSecretKeyPath
+    putStrLn "Blockchain = DEAD"
+    undefined sk

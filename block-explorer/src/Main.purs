2ee3de65e3dba0668fc20eafa7630d5540972e2d
Ante Kegalj <akegalj@gmail.com>
2016-07-08 00:56:54 +0200

    Non working thermite counter example

--- block-explorer/src/Main.purs
+++ block-explorer/src/Main.purs
@@ -1,9 +1,41 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Thermite as T
+
+import React as R
+import React.DOM as R
+import React.DOM.Props as RP
+import ReactDOM as RDOM
+
+
+data Action = Increment | Decrement
+
+type State = { counter :: Int }
+
+initialState :: State
+initialState = { counter: 0 }
+
+render :: T.Render State _ Action
+render dispatch _ state _ =
+    [ R.p' [ R.text "Value: "
+           , R.text $ show state.counter
+           ]
+    , R.p' [ R.button [ RP.onClick \_ -> dispatch Increment ]
+                      [ R.text "Increment" ]
+           , R.button [ RP.onClick \_ -> dispatch Decrement ]
+                      [ R.text "Decrement" ]
+           ]
+    ]
+
+performAction :: T.PerformAction _ State _ Action
+performAction Increment _ _ = void $ T.cotransform $ \state -> state { counter = state.counter + 1 }
+performAction Decrement _ _ = void $ T.cotransform $ \state -> state { counter = state.counter - 1 }
+
+spec :: T.Spec _ State _ Action
+spec = T.simpleSpec performAction render
+
 main = do
-  log "Hello sailor!"
+    let component = T.createClass spec initialState
+    body >>= RDOM.render (R.createFactory component {}) -- this is not working

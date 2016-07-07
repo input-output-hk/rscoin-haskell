7b8573062c57ade46665d5f3f1647e30141e22bc
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-07 18:51:28 +0300

    Add Explorer.Server with some logic but w/o actual serving

---
+++ src/RSCoin/Explorer/Server.hs
@@ -0,0 +1,30 @@
+-- | Explorer Server.
+
+module RSCoin.Explorer.Server
+       ( serve
+       ) where
+
+
+import           Data.Acid.Advanced        (query', update')
+
+import qualified RSCoin.Core               as C
+import           RSCoin.Timed              (WorkMode)
+
+import           RSCoin.Explorer.AcidState (AddHBlock (..),
+                                            GetLastPeriodId (..), State)
+
+-- TODO: we need encrypted communication, so this server is dummy for now.
+serve
+    :: WorkMode m
+    => Int -> State -> C.SecretKey -> m ()
+serve port _ _ = do
+    C.serve port []
+
+-- handleNewHBlock
+--     :: WorkMode m
+--     => State -> C.PeriodId -> C.HBlock -> m C.PeriodId
+-- handleNewHBlock st newBlockId newBlock = do
+--     expectedPid <- maybe 0 succ <$> query' st GetLastPeriodId
+--     if expectedPid == newBlockId
+--         then update' st (AddHBlock newBlockId newBlock) >> return newBlockId
+--         else return expectedPid

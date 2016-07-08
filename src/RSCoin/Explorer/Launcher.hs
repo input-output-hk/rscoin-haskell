fdff99bf25e36ff4264c3bad225a877dce68700b
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-09 01:56:59 +0300

    Use warp server in Explorer

--- src/RSCoin/Explorer/Launcher.hs
+++ src/RSCoin/Explorer/Launcher.hs
@@ -10,12 +10,15 @@ module RSCoin.Explorer.Launcher
 import           Control.Monad.Catch       (bracket)
 import           Control.Monad.Trans       (liftIO)
 import           Data.ByteString           (ByteString)
+import           Network.Wai.Handler.Warp  (run)
 
 import           RSCoin.Core               (SecretKey)
-import           RSCoin.Timed              (MsgPackRpc, WorkMode, runRealMode)
+import           RSCoin.Timed              (MsgPackRpc, WorkMode, fork_,
+                                            runRealMode)
 
 import           RSCoin.Explorer.AcidState (State, closeState, openState)
 import           RSCoin.Explorer.Server    (serve)
+import qualified RSCoin.Explorer.Web       as Web
 
 explorerWrapperReal :: ByteString -> FilePath -> (State -> MsgPackRpc a) -> IO a
 explorerWrapperReal bankHost storagePath =
@@ -24,7 +27,10 @@ explorerWrapperReal bankHost storagePath =
 
 launchExplorerReal :: ByteString -> Int -> FilePath -> SecretKey -> IO ()
 launchExplorerReal bankHost port storagePath sk =
-    explorerWrapperReal bankHost storagePath $ launchExplorer port sk
+    explorerWrapperReal bankHost storagePath $
+    \st ->
+         do fork_ $ launchExplorer port sk st
+            liftIO $ run (port + 1) $ Web.application st
 
 launchExplorer
     :: WorkMode m

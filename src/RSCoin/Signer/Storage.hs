69e66aac77da419dadd45d109ce7b0e2f9b3bbce
georgeee <george.agapov@gmail.com>
2016-07-08 22:23:27 +0300

    [RSC-65] add comments to Signer's interfaces (RSCoin::Signer::Storage)

--- src/RSCoin/Signer/Storage.hs
+++ src/RSCoin/Signer/Storage.hs
@@ -33,12 +33,14 @@ import           RSCoin.Core         (AddrId, Address, AddressStrategyMap,
                                       validateSignature)
 
 data Storage = Storage
-    { _txPool         :: M.Map Address (M.Map Transaction (M.Map Address Signature)) -- pool of trasactions to be signed
-    , _addresses      :: AddressStrategyMap
-    , _unspentAddrIds :: M.Map Address (S.Set AddrId)
-    , _utxo           :: Utxo
-    , _txPoolAddrIds  :: M.Map AddrId (S.Set (Address, Transaction))
-    , _periodId       :: PeriodId
+    { _txPool         :: M.Map Address (M.Map Transaction (M.Map Address Signature))
+      -- ^ Pool of trasactions to be signed, already collected signatures
+    , _addresses      :: AddressStrategyMap -- ^ Non-default addresses, registered in system (published to bank)
+    , _unspentAddrIds :: M.Map Address (S.Set AddrId) -- ^ Mapping between address and a set of unspent addrids, owned by it
+    , _utxo           :: Utxo -- ^ Mapping between addrid and address
+    , _txPoolAddrIds  :: M.Map AddrId (S.Set (Address, Transaction)) -- ^ Mapping between addrid and all pairs (addr, transaction),
+                                                                     --   being kept in `txPool`, such that addrid serve as an input for transaction
+    , _periodId       :: PeriodId -- ^ Last periodId, known to signer
     } deriving Show
 
 $(makeLenses ''Storage)
@@ -46,6 +48,7 @@ $(makeLenses ''Storage)
 emptySignerStorage :: Storage
 emptySignerStorage = Storage M.empty M.empty M.empty M.empty M.empty (-1)
 
+-- Erase occurrences published (address, transaction) from storage
 forgetAddrTx :: Address -> Transaction -> Update Storage ()
 forgetAddrTx addr tx = do
   txPool %= M.update (ifNotEmpty . M.delete tx) addr
@@ -62,8 +65,11 @@ getStrategy addr = fromMaybe DefaultStrategy . M.lookup addr <$> use addresses
 instance MonadThrow (Update s) where
     throwM = throw
 
+-- Receives tx, addr, (addr, sig) pair, checks validity and publishes (tx, addr) to storage,
+--  adds (addr, sig) to list of already collected for particular (tx, addr) pair.
 addSignature :: Transaction -> Address -> (Address, Signature) -> Update Storage ()
 addSignature tx addr sg@(sigAddr, sig) = do
+    -- @TODO check transaction correctness, i.e. equality of sums and etc.
     checkAddrIdsKnown
     checkAddrRelativeToTx
     checkSigRelativeToAddr
@@ -95,6 +101,9 @@ addSignature tx addr sg@(sigAddr, sig) = do
         when (not $ validateSignature sig sigAddr tx) $
           throwM SEInvalidSignature
 
+-- By given (tx, addr) retreives list of collected signatures.
+-- If list is complete enough to complete strategy, (tx, addr) pair
+--  and all corresponding data occurrences get removed from Storage
 acquireSignatures :: Transaction -> Address -> Update Storage [(Address, Signature)]
 acquireSignatures tx addr = do
     sgs <- liftQuery (getSignatures tx addr)
@@ -103,12 +112,16 @@ acquireSignatures tx addr = do
         forgetAddrTx addr tx
     return sgs
 
+-- By given (tx, addr) get list of collected signatures (or empty list if (tx, addr) is not registered/already removed from Signer)
+-- Read-only method
 getSignatures :: Transaction -> Address -> Query Storage [(Address, Signature)]
 getSignatures tx addr = maybe [] M.assocs . (M.lookup tx <=< M.lookup addr) <$> view txPool
 
+-- Get last known periodId of Signer (interface for bank)
 getPeriodId :: Query Storage PeriodId
 getPeriodId = view periodId
 
+-- Announce HBlocks, not yet known to signer
 announceNewPeriods :: PeriodId -- periodId of latest hblock
                    -> [HBlock] -- blocks, head corresponds to the latest block
                    -> Update Storage ()

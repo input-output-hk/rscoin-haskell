177ed50ecf8d3edb0a07c89728c546f3f3975d96
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-09 15:14:27 +0300

    Add very stupid Explorer ws server

--- src/RSCoin/Explorer/Web.hs
+++ src/RSCoin/Explorer/Web.hs
@@ -10,22 +10,24 @@ module RSCoin.Explorer.Web
        ( application
        ) where
 
-import           Control.Monad.Catch       (Exception, catch, throwM)
-import           Control.Monad.Except      (throwError)
-import           Control.Monad.Reader      (ReaderT, ask, runReaderT)
-import           Control.Monad.Trans       (liftIO)
-import           Data.Acid.Advanced        (query')
-import           Data.Typeable             (Typeable)
-import           Network.Wai               (Application)
-import           Servant                   ((:>), (:~>) (Nat), Capture,
-                                            FromHttpApiData (parseUrlPiece),
-                                            Get, Handler, JSON, Proxy (Proxy),
-                                            ServerT, enter, err404,
-                                            parseUrlPiece, serve)
+import           Control.Monad.Catch            (Exception, catch, throwM)
+import           Control.Monad.Except           (throwError)
+import           Control.Monad.Reader           (ReaderT, ask, runReaderT)
+import           Control.Monad.Trans            (liftIO)
+import           Data.Acid.Advanced             (query')
+import           Data.Text                      (Text)
+import           Data.Typeable                  (Typeable)
+import           Network.Wai                    (Application)
+import           Network.Wai.Handler.WebSockets (websocketsOr)
+import qualified Network.WebSockets             as WS
+import           Servant                        ((:>), (:~>) (Nat), Capture, FromHttpApiData (parseUrlPiece),
+                                                 Get, Handler, JSON,
+                                                 Proxy (Proxy), ServerT, enter,
+                                                 err404, parseUrlPiece, serve)
 
-import qualified RSCoin.Core               as C
+import qualified RSCoin.Core                    as C
 
-import           RSCoin.Explorer.AcidState (GetTx (..), State)
+import           RSCoin.Explorer.AcidState      (GetTx (..), State)
 
 type ExplorerApi =
     "tx" :> Capture "txid" C.TransactionId :> Get '[JSON] C.Transaction
@@ -57,8 +59,17 @@ handleGetTx i = maybe (throwM NotFound) pure =<< flip query' (GetTx i) =<< ask
 servantServer :: ServerT ExplorerApi MyHandler
 servantServer = handleGetTx
 
-application :: State -> Application
-application st = serve explorerApi $ enter nat servantServer
+servantApp :: State -> Application
+servantApp st = serve explorerApi $ enter nat servantServer
   where
     nat :: MyHandler :~> Handler
     nat = Nat $ convertHandler st
+
+wsApp :: WS.ServerApp
+wsApp pendingConn = do
+    conn <- WS.acceptRequest pendingConn
+    WS.forkPingThread conn 30
+    WS.sendTextData conn ("top kek" :: Text)
+
+application :: State -> Application
+application st = websocketsOr WS.defaultConnectionOptions wsApp $ servantApp st

8c9fed75a0005e69c33e66980f9b239db62182aa
Dmitry Kovanikov <kovanikov@gmail.com>
2016-07-10 14:50:40 +0300

    Minor: Notary Error negligible refactoring

--- src/RSCoin/Notary/Error.hs
+++ src/RSCoin/Notary/Error.hs
@@ -22,9 +22,9 @@ data NotaryError
     = NEAddrNotRelativeToTx        -- ^ Address doesn't correspond to any of transaction's inputs
     | NEAddrIdNotInUtxo PeriodId   -- ^ One of transaction's addrId is not present in utxo
                                    --   PeriodId supplied -- actual periodId known to Notary
+    | NEInvalidSignature           -- ^ Invalid signature provided
     | NEStrategyNotSupported Text  -- ^ Address's strategy is not supported, with name provided
     | NEUnrelatedSignature         -- ^ Signature provided doesn't correspond to any of address' parties
-    | NEInvalidSignature           -- ^ Invalid signature provided
     deriving (Eq, Show, Typeable)
 
 instance Exception NotaryError where
@@ -34,9 +34,9 @@ instance Exception NotaryError where
 instance Buildable NotaryError where
     build NEAddrNotRelativeToTx      = "NEAddrNotRelativeToTx"
     build (NEAddrIdNotInUtxo pId)    = bprint ("NEAddrIdNotInUtxo, notary's periodId " % int) pId
+    build NEInvalidSignature         = "NEInvalidSignature"
     build (NEStrategyNotSupported s) = bprint ("NEStrategyNotSupported, strategy " % stext) s
     build NEUnrelatedSignature       = "NEUnrelatedSignature"
-    build NEInvalidSignature         = "NEInvalidSignature"
 
 toObj
     :: MessagePack a
@@ -46,16 +46,16 @@ toObj = toObject
 instance MessagePack NotaryError where
     toObject NEAddrNotRelativeToTx      = toObj (0, ())
     toObject (NEAddrIdNotInUtxo pId)    = toObj (1, pId)
-    toObject (NEStrategyNotSupported s) = toObj (2, s)
-    toObject NEUnrelatedSignature       = toObj (3, ())
-    toObject NEInvalidSignature         = toObj (4, ())
+    toObject NEInvalidSignature         = toObj (2, ())
+    toObject (NEStrategyNotSupported s) = toObj (3, s)
+    toObject NEUnrelatedSignature       = toObj (4, ())
 
     fromObject obj = do
         (i, payload) <- fromObject obj
         case (i :: Int) of
             0 -> pure NEAddrNotRelativeToTx
             1 -> NEAddrIdNotInUtxo      <$> fromObject payload
-            2 -> NEStrategyNotSupported <$> fromObject payload
-            3 -> pure NEUnrelatedSignature
-            4 -> pure NEInvalidSignature
+            2 -> pure NEInvalidSignature
+            3 -> NEStrategyNotSupported <$> fromObject payload
+            4 -> pure NEUnrelatedSignature
             _ -> Nothing

7940d67400282c6d56de8853207148f69227b936
Ante Kegalj <akegalj@gmail.com>
2016-07-11 19:32:06 +0200

    Add purescript-bridge dep. Cleanup

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,17 @@
-import           Distribution.Simple (defaultMainWithHooks, simpleUserHooks, UserHooks (postBuild))
-import           System.Process      (system)
+import           Distribution.Simple        (defaultMainWithHooks, simpleUserHooks,
+                                             UserHooks (postBuild))
+import           System.Process             (system)
 
-import           Control.Monad       (void)
+import           Control.Monad              (void)
+
+import           RSCoin.Core
+import           Language.PureScript.Bridge (writePSTypes, buildBridge,
+                                             defaultBridge)
+
 
 main = defaultMainWithHooks $ simpleUserHooks { postBuild = buildPureScript }
 
-buildPureScript _ _ _ _ = void $ system "touch bla"
+buildPureScript _ _ _ _ = do
+    writePSTypes "" (buildBridge defaultBridge)
+        [ mkSumType (Proxy :: Proxy IncomingMsg)
+        ]

69e7b2b21b14e0d4f571c4cebaea344a37fdcdc9
Ante Kegalj <akegalj@gmail.com>
2016-07-11 14:17:22 +0200

    Add initial custom install hook (for frontend)

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,10 @@
-import Distribution.Simple
-main = defaultMain
+import           Distribution.Simple
+import           Distribution.Simple.LocalBuildInfo
+import           System.Process
+import           System.Exit
+
+import           Control.Monad (void)
+
+main = defaultMainWithHooks $ simpleUserHooks { postBuild = buildPureScript }
+
+buildPureScript _ _ _ _ = void $ system "touch bla"

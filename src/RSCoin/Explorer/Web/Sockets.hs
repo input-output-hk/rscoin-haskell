de43ed0093e03b555eb701ef19256ca915d8dae0
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-11 16:19:55 +0300

    Refactor and document types in Web.Sockets

--- src/RSCoin/Explorer/Web/Sockets.hs
+++ src/RSCoin/Explorer/Web/Sockets.hs
@@ -10,11 +10,16 @@ module RSCoin.Explorer.Web.Sockets
        ( wsApp
        ) where
 
+import           Control.Lens              (makeLenses, view)
 import           Control.Monad             (forever)
+import           Control.Monad.Reader      (ReaderT, runReaderT)
+import           Control.Monad.Trans       (MonadIO (liftIO))
 import           Data.Acid.Advanced        (query')
-import           Data.Aeson                (ToJSON (toJSON), eitherDecode,
-                                            encode)
-import           Data.Aeson.TH             (deriveFromJSON, deriveToJSON)
+import           Data.Aeson                (FromJSON, ToJSON (toJSON),
+                                            eitherDecode, encode)
+import           Data.Aeson.TH             (deriveFromJSON, deriveJSON,
+                                            deriveToJSON)
+import qualified Data.ByteString.Lazy      as BSL
 import           Data.Either.Combinators   (mapLeft)
 import qualified Data.Map                  as M
 import           Data.Text                 (Text, pack)
@@ -26,62 +31,107 @@ import qualified RSCoin.Core               as C
 
 import           RSCoin.Explorer.AcidState (GetAddressCoins (..), State)
 
-data IncomingMsg
-    = IMGetAddressInfo C.Address
-    | IMGetBalance
+-- | Run-time errors which may happen within this server.
+data ServerError =
+    ParseError Text
     deriving (Show)
 
-$(deriveFromJSON defaultOptions ''IncomingMsg)
+$(deriveJSON leaveTagOptions ''ServerError)
 
-instance WS.WebSocketsData (Either ErrorMsg IncomingMsg) where
-    fromLazyByteString = mapLeft (ParseError . pack) . eitherDecode
-    toLazyByteString = undefined
+type ErrorableMsg msg = Either ServerError msg
 
-data ErrorMsg
-    = ParseError Text
-    | UnexpectedMessage Text
+-- | Communication starts with Introductory Message sent by
+-- client. This type describes all such messages. Introductiory
+-- message starts communication between server and client about some
+-- topic (e. g. about particular address).
+data IntroductoryMsg =
+    -- | AddressInfo starts communication about given Address. Within
+    -- this communication user can request various data about address.
+    IMAddressInfo C.Address
     deriving (Show)
 
-$(deriveToJSON leaveTagOptions ''ErrorMsg)
+$(deriveFromJSON defaultOptions ''IntroductoryMsg)
 
-instance WS.WebSocketsData ErrorMsg where
-    fromLazyByteString = undefined
-    toLazyByteString = encode
+customDecode
+    :: FromJSON a
+    => BSL.ByteString -> Either ServerError a
+customDecode = mapLeft (ParseError . pack) . eitherDecode
 
-data OutcomingMsg =
-    OMBalance C.CoinsMap
+instance WS.WebSocketsData (ErrorableMsg IntroductoryMsg) where
+    fromLazyByteString = customDecode
+    toLazyByteString = error "Attempt to serialize IntroductoryMsg is illegal"
+
+-- | Within communication started with AddressInfo message client can
+-- send messages defined by this type.
+data AddressInfoMsg =
+    -- | GetBalance message requests balance of address associated
+    -- with connection.
+    AIGetBalance
+    deriving (Show)
+
+$(deriveFromJSON defaultOptions ''AddressInfoMsg)
+
+instance WS.WebSocketsData (ErrorableMsg AddressInfoMsg) where
+    fromLazyByteString = customDecode
+    toLazyByteString = error "Attempt to serialize AddressInfoMsg is illegal"
+
+-- | This type contains all possible messages sent by this server.
+data OutcomingMsg
+    =
+      -- | Sent in case of error.
+      OMError ServerError
+    |
+      -- | Sent in response to `AddressInfo` message.
+      OMBalance C.CoinsMap
     deriving (Show)
 
 instance ToJSON C.CoinsMap where
     toJSON = toJSON . M.assocs
 
-$(deriveToJSON leaveTagOptions ''OutcomingMsg)
+$(deriveToJSON defaultOptions ''OutcomingMsg)
 
 instance WS.WebSocketsData OutcomingMsg where
-    fromLazyByteString = undefined
+    fromLazyByteString = error "Attempt to deserialize OutcominMsg is illegal"
     toLazyByteString = encode
 
-wsApp :: State -> WS.ServerApp
-wsApp st pendingConn = do
-    conn <- WS.acceptRequest pendingConn
-    WS.forkPingThread conn 30
-    msg <- WS.receiveData conn
-    case msg of
-        Left err -> WS.sendTextData conn (err :: ErrorMsg)
-        Right (IMGetAddressInfo addr) -> startNegotiation st addr conn
-        Right _ ->
-            WS.sendTextData conn $
-            UnexpectedMessage
-                "Negotiation must start with GetAddressInfo message"
+data ServerState = ServerState
+    { _ssDataBase :: State
+    }
 
-startNegotiation :: State -> C.Address -> WS.Connection -> IO ()
-startNegotiation st addr conn =
-    forever $
-    do msg <- WS.receiveData conn
-       case msg of
-           Left err -> WS.sendTextData conn (err :: ErrorMsg)
-           Right IMGetBalance ->
-               WS.sendTextData conn . OMBalance =<<
-               query' st (GetAddressCoins addr)
-           Right _ ->
-               WS.sendTextData conn $ UnexpectedMessage "Unexpected message"
+$(makeLenses ''ServerState)
+
+type Handler = ReaderT ServerState IO
+
+send
+    :: MonadIO m
+    => WS.Connection -> OutcomingMsg -> m ()
+send conn = liftIO . WS.sendTextData conn
+
+recv
+    :: (MonadIO m, WS.WebSocketsData (ErrorableMsg msg))
+    => WS.Connection -> (msg -> m ()) -> m ()
+recv conn callback =
+    either (send conn . OMError) callback =<<
+    liftIO (WS.receiveData conn)
+
+handler :: WS.PendingConnection -> Handler ()
+handler pendingConn = do
+    conn <- liftIO $ WS.acceptRequest pendingConn
+    liftIO $ WS.forkPingThread conn 30
+    recv conn $ onReceive conn
+  where
+    onReceive conn (IMAddressInfo addr) = startNegotiation addr conn
+
+startNegotiation :: C.Address -> WS.Connection -> Handler ()
+startNegotiation addr conn = forever $ recv conn onReceive
+  where
+    onReceive AIGetBalance =
+        send conn . OMBalance =<<
+        flip query' (GetAddressCoins addr) =<< view ssDataBase
+
+-- | Given access to Explorer's data base, returns WebSockets server
+-- application.
+wsApp :: State -> WS.ServerApp
+wsApp st = flip runReaderT ss . handler
+  where
+    ss = ServerState st

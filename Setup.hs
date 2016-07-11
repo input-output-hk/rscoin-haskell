97b935cc17b89e0b633b8baaade377a85752020a
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-12 01:24:27 +0300

    Use postReg instead of postBuild in Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,11 +1,16 @@
-import           Distribution.Simple (defaultMainWithHooks, simpleUserHooks,
-                                      UserHooks (postBuild))
+import           Distribution.Simple (UserHooks (postReg), defaultMainWithHooks,
+                                      simpleUserHooks)
+import           System.Exit         (ExitCode (..), exitWith)
 import           System.Process      (system)
-import           System.Exit         (exitWith, ExitCode (..))
 
 main :: IO ()
-main = defaultMainWithHooks $ simpleUserHooks { postBuild = hs2purs }
-  where hs2purs _ _ _ _ =
-            system "stack $NIX_STACK exec -- rscoin-hs2purs" >>= printError
-        printError ExitSuccess = return ()
-        printError e           = exitWith e
+main =
+    defaultMainWithHooks $
+    simpleUserHooks
+    { postReg = hs2purs
+    }
+  where
+    hs2purs _ _ _ _ =
+        system "stack $NIX_STACK exec -- rscoin-hs2purs" >>= printError
+    printError ExitSuccess = return ()
+    printError e = exitWith e

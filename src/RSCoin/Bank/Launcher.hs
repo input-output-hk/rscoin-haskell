cb2df2de939a3ba9055f525545e9f1e83bd9c748
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-13 18:15:19 +0300

    Document functions in Bank.Launcher module

--- src/RSCoin/Bank/Launcher.hs
+++ src/RSCoin/Bank/Launcher.hs
@@ -18,12 +18,14 @@ import           Data.Acid.Advanced    (update')
 import           Data.Functor          (void)
 import           Data.Time.Units       (TimeUnit)
 
-import           RSCoin.Core           (Address, Explorer, Mintette, PeriodId, PublicKey,
-                                        SecretKey, Strategy, defaultLayout')
+import           RSCoin.Core           (Address, Explorer, Mintette, PeriodId,
+                                        PublicKey, SecretKey, Strategy,
+                                        defaultLayout')
 import           RSCoin.Timed          (MsgPackRpc, PlatformLayout, ThreadId,
                                         WorkMode, fork, killThread, runRealMode)
 
-import           RSCoin.Bank.AcidState (AddAddress (AddAddress), AddExplorer (AddExplorer),
+import           RSCoin.Bank.AcidState (AddAddress (AddAddress),
+                                        AddExplorer (AddExplorer),
                                         AddMintette (AddMintette), State,
                                         closeState, openState)
 import           RSCoin.Bank.Server    (serve)
@@ -34,10 +36,14 @@ bankWrapperReal layout storagePath =
     runRealMode layout .
     bracket (liftIO $ openState storagePath) (liftIO . closeState)
 
-launchBankReal :: (TimeUnit t) => PlatformLayout -> t -> FilePath -> SecretKey -> IO ()
+-- | Launch Bank in real mode. This function works indefinitely.
+launchBankReal
+    :: (TimeUnit t)
+    => PlatformLayout -> t -> FilePath -> SecretKey -> IO ()
 launchBankReal layout periodDelta storagePath sk =
     bankWrapperReal layout storagePath $ void . launchBank periodDelta sk
 
+-- | Launch Bank in any WorkMode. This function works indefinitely.
 launchBank
     :: (TimeUnit t, WorkMode m)
     => t -> SecretKey -> State -> m ThreadId
@@ -49,14 +55,17 @@ launchBank periodDelta sk st = do
         killThread tId
         fork $ runWorkerWithPeriod periodDelta sk st
 
-addAddressIO :: FilePath -> Address -> Strategy -> IO ()
-addAddressIO storagePath a s =
-    bankWrapperReal (defaultLayout' "127.0.0.1") storagePath $ flip update' (AddAddress a s)
-
+-- | Add mintette to Bank inside IO Monad.
 addMintetteIO :: FilePath -> Mintette -> PublicKey -> IO ()
 addMintetteIO storagePath m k =
     bankWrapperReal (defaultLayout' "127.0.0.1") storagePath $ flip update' (AddMintette m k)
 
+-- | Add address to Bank inside IO Monad.
+addAddressIO :: FilePath -> Address -> Strategy -> IO ()
+addAddressIO storagePath a s =
+    bankWrapperReal (defaultLayout' "127.0.0.1") storagePath $ flip update' (AddAddress a s)
+
+-- | Add explorer to Bank inside IO Monad.
 addExplorerIO :: FilePath -> Explorer -> PeriodId -> IO ()
 addExplorerIO storagePath e pId =
     bankWrapperReal (defaultLayout' "127.0.0.1") storagePath $ flip update' (AddExplorer e pId)

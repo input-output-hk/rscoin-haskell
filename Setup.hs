8b28aba127ab19924af56518e654dc8e6d26a515
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-13 20:51:20 +0300

    Disable automatic frontend build

--- Setup.hs
+++ Setup.hs
@@ -13,8 +13,8 @@ main =
     hs2purs _ _ _ _ = do
         system "stack $NIX_STACK exec -- rscoin-hs2purs" >>= printError
         -- FIXME: https://gitlab.serokell.io/rscoin/rscoin/commit/fc8d36123dba122a4ce41053e1881ea5e1873030#note_1193
-        system
-            "cd block-explorer; PATH=$PATH:node_modules/.bin; npm install; bower install; cd websocket-example; PATH=$PATH:../node_modules/.bin; bower install; pulp test" >>=
-            printError
+        -- system
+        --     "cd block-explorer; PATH=$PATH:node_modules/.bin; npm install; bower install; cd websocket-example; PATH=$PATH:../node_modules/.bin; bower install; pulp test" >>=
+        --     printError
     printError ExitSuccess = return ()
     printError e = exitWith e

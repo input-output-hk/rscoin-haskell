9e2528607069b21f8b2498e25650446fa2f8d300
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-07-17 00:43:56 +0100

    Add test to verifyChain

--- test/Test/RSCoin/Core/CryptoSpec.hs
+++ test/Test/RSCoin/Core/CryptoSpec.hs
@@ -7,8 +7,9 @@ module Test.RSCoin.Core.CryptoSpec
 import           Test.Hspec            (Spec, describe)
 import           Test.Hspec.QuickCheck (prop)
 
-import           RSCoin.Core.Crypto    (PublicKey, SecretKey, checkKeyPair,
-                                        derivePublicKey, sign, verify)
+import           RSCoin.Core.Crypto    (PublicKey, SecretKey,
+                                        checkKeyPair, derivePublicKey, sign,
+                                        verify, verifyChain)
 
 spec :: Spec
 spec =
@@ -19,6 +20,9 @@ spec =
     prop
         "Signature generated using secret key may be verified only by public key `pk` for which `checkKeyPair (sk, pk)`"
         signThenVerifyArbitraryPK
+    prop
+        "Signatures generated in chain using secret keys may be verified using derived public keys"
+        signThenVerifyArbitraryChain
 
 signThenVerify :: Int -> SecretKey -> Bool
 signThenVerify v sk = verify (derivePublicKey sk) (sign sk v) v
@@ -26,3 +30,11 @@ signThenVerify v sk = verify (derivePublicKey sk) (sign sk v) v
 signThenVerifyArbitraryPK :: Int -> SecretKey -> PublicKey -> Bool
 signThenVerifyArbitraryPK v sk pk =
     verify pk (sign sk v) v == checkKeyPair (sk, pk)
+
+signThenVerifyArbitraryChain :: SecretKey -> [SecretKey] -> Bool
+signThenVerifyArbitraryChain sk l = verifyChain pk $ helper sk l
+    where pk = derivePublicKey sk
+          helper _ [] = []
+          helper sKey (newSk:rest) =
+              let newPk = (derivePublicKey newSk)
+              in  (sign sKey newPk, newPk) : helper newSk rest

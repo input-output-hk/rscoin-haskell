0a3770bb99389235c9d6bed24d48c6de1c1b9745
Ante Kegalj <akegalj@gmail.com>
2016-07-18 19:27:49 +0200

    Send intro message on button click

--- block-explorer/src/Layout.purs
+++ block-explorer/src/Layout.purs
@@ -1,23 +1,28 @@
 module App.Layout where
 
-import Prelude                   (($), map, (<<<), const)
+import Prelude                     (($), map, (<<<), const, pure)
 
-import App.Counter               as Counter
-import App.NotFound              as NotFound
-import App.Routes                (Route(Home, NotFound))
-import App.Connection            (Connection, Action (..), WEBSOCKET) as C
-import App.RSCoin                (emptyAddress, Address, newAddress,
-                                  addressToString, IntroductoryMsg (..))
+import App.Counter                 as Counter
+import App.NotFound                as NotFound
+import App.Routes                  (Route(Home, NotFound))
+import App.Connection              (Connection, Action (..), WEBSOCKET, introMessage) as C
+import App.RSCoin                  (emptyAddress, Address, newAddress,
+                                    addressToString, IntroductoryMsg (..))
 
-import Data.Maybe                (Maybe (..))
+import Data.Maybe                  (Maybe (..), fromJust)
 
-import Pux                       (EffModel, noEffects)
-import Pux.Html                  (Html, div, h1, p, text, input, button)
-import Pux.Html.Attributes       (type_, value)
-import Pux.Html.Events           (onChange, onClick)
+import Pux                         (EffModel, noEffects, onlyEffects)
+import Pux.Html                    (Html, div, h1, p, text, input, button)
+import Pux.Html.Attributes         (type_, value)
+import Pux.Html.Events             (onChange, onClick)
 
-import Control.Monad.Eff.Console (CONSOLE)
-import DOM                       (DOM)
+import Control.Apply               ((*>))
+
+import DOM                         (DOM)
+import Control.Monad.Eff.Console   (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION, catchException)
+
+import Partial.Unsafe              (unsafePartial)
 
 data Action
     = PageView Route
@@ -40,9 +45,14 @@ init =
     , address: emptyAddress
     }
 
-update :: Action -> State -> EffModel State Action (console :: CONSOLE, ws :: C.WEBSOCKET, dom :: DOM)
+update :: forall eff. Action -> State -> EffModel State Action (console :: CONSOLE, ws :: C.WEBSOCKET, err :: EXCEPTION | eff)
 update (PageView route) state = noEffects $ state { route = route }
 update (SocketAction (C.ReceivedData msg)) state = noEffects state
+update (SocketAction (C.SendIntroData msg)) state =
+    onlyEffects state
+        [ C.introMessage (unsafePartial $ fromJust state.socket) msg *> pure Nop
+        -- FIXME: if socket isn't opened open some error page
+        ]
 update (SocketAction _) state = noEffects state
 update (AddressChange address) state = noEffects $ state { address = address }
 update Nop state = noEffects state

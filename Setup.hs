2303c9566f7cb893d0c515a122ce004b5d7797e9
Ante Kegalj <akegalj@gmail.com>
2016-07-21 01:29:43 +0200

    Build frontend for production deployment

--- Setup.hs
+++ Setup.hs
@@ -14,7 +14,7 @@ main =
         system "stack $NIX_STACK exec -- rscoin-hs2purs" >>= printError
         -- FIXME: https://gitlab.serokell.io/rscoin/rscoin/commit/fc8d36123dba122a4ce41053e1881ea5e1873030#note_1193
         system
-            "cd block-explorer; PATH=$PATH:node_modules/.bin; npm install; pulp test" >>=
+            "cd block-explorer; PATH=$PATH:node_modules/.bin; npm install; npm run test; npm run build" >>=
             printError
     printError ExitSuccess = return ()
     printError e = exitWith e

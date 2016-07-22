e9408ea27c77af638ce7c52a626ce85d96b6c59c
Dmitry Kovanikov <kovanikov@gmail.com>
2016-07-22 03:27:15 +0300

    Add rscoin-explorer alias

--- admin/aliases.sh
+++ admin/aliases.sh
@@ -2,3 +2,4 @@ alias rscoin-mintette="stack $NIX_STACK exec -- rscoin-mintette --log-severity D
 alias rscoin-bank="stack $NIX_STACK exec -- rscoin-bank --log-severity Debug"
 alias rscoin-user="stack $NIX_STACK exec -- rscoin-user"
 alias rscoin-notary="stack $NIX_STACK exec -- rscoin-notary --log-severity Debug"
+alias rscoin-explorer="stack $NIX_STACK exec -- rscoin-explorer --log-severity Debug"

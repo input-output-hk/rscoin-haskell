778ca1df0fb9b24d130003b2c8f8c7a3ea2fcd1f
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-07-22 06:24:08 +0300

    Add withStack option to some admin/ scripts

--- admin/initBankUser.sh
+++ admin/initBankUser.sh
@@ -1,3 +1,7 @@
-rscoin-user --bank-mode update
-rscoin-user --bank-mode list
-
+if [ -z $1 ]; then
+  rscoin-user --bank-mode update
+  rscoin-user --bank-mode list
+else
+  stack exec rscoin-user -- --bank-mode update
+  stack exec rscoin-user -- --bank-mode list
+fi

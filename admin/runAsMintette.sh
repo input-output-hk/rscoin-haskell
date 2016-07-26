d7758a26bb15781503844932e97a59404173a751
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-07-26 23:22:20 +0300

    [RSC-94] Refactor default mintette port settings
    Now scripts use keys in the way mintettes have ports from
    3100 to 3199 (previously it was different).

--- admin/runAsMintette.sh
+++ admin/runAsMintette.sh
@@ -4,18 +4,26 @@
 
 test -z $1 && echo "No first argument" && exit
 
+test $1 -ge 100 -o $1 -lt 0 && echo "No mintette number within range [0..99] is allowed" && exit 
+
+if [ $1 -lt 10 ]; then
+  mintNum="0$1"
+else
+  mintNum=$1
+fi
+
 if uname -a | grep -i nixos > /dev/null; then
   export NIX_STACK="--nix"
 fi
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mpath=$dir/mintetteKeys/mintette$1/
+mpath=$dir/mintetteKeys/mintette$mintNum
 
 if [ -z $2 ]; then
-  sleep 2 && rscoin-bank --log-severity Debug add-mintette --host 127.0.0.1 --port 311$1 --key $(cat $mpath/mintette$1.pub)&
-  rscoin-mintette --log-severity Debug --path "$mpath/mintette-db" --sk $mpath/mintette$1.sec -p 311$1
+  sleep 2 && rscoin-bank --log-severity Debug add-mintette --host 127.0.0.1 --port 31$mintNum --key $(cat $mpath/mintette$mintNum.pub)&
+  rscoin-mintette --log-severity Debug --path "$mpath/mintette-db" --sk $mpath/mintette$mintNum.sec -p 31$mintNum
 else
-  sleep 2 && stack $NIX_STACK exec rscoin-bank -- --log-severity Debug add-mintette --host 127.0.0.1 --port 311$1 --key $(cat $mpath/mintette$1.pub)&
-  stack $NIX_STACK exec rscoin-mintette -- --log-severity Debug --path "$mpath/mintette-db" --sk $mpath/mintette$1.sec -p 311$1
+  sleep 2 && stack $NIX_STACK exec rscoin-bank -- --log-severity Debug add-mintette --host 127.0.0.1 --port 31$mintNum --key $(cat $mpath/mintette$mintNum.pub)&
+  stack $NIX_STACK exec rscoin-mintette -- --log-severity Debug --path "$mpath/mintette-db" --sk $mpath/mintette$mintNum.sec -p 31$mintNum
 fi

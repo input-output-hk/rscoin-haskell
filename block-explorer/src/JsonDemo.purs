2bdb15a9dc56dbf84da9131260fc37f91844deb6
Ante Kegalj <akegalj@gmail.com>
2016-07-27 14:54:40 +0200

    Fix bridge types

--- block-explorer/src/JsonDemo.purs
+++ block-explorer/src/JsonDemo.purs
@@ -27,16 +27,18 @@ helper v = do
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-    let coin     = W.Coin {getColor: W.Color {getC: 0}, getCoin: W.CoinAmount "0.3242342"}
-        key      = W.PublicKey "YblQ7+YCmxU/4InsOwSGH4Mm37zGjgy7CLrlWlnHdnM="
-        hash     = W.Hash "DldRwCblQ7Loqy6wYJnaodHl30d3j3eH+qtFzfEv46g="
-        addr     = W.Address { getAddress:  key }
-        tx       = W.TransactionSummarySerializable {txId: hash, txInputs: [tuple4 hash 0 coin $ Just addr], txOutputs: [(Tuple addr coin)], txInputsSum: [Tuple 0 coin], txOutputsSum: [Tuple 0 coin], txInputsTotal: W.CoinAmount "12", txOutputsTotal: W.CoinAmount "12"}
-        err      = W.ParseError "error"
-        introMsg = W.IMAddressInfo addr
-        aiMsg    = W.AIGetTransactions (Tuple 0 2)
-        outMsg   = W.OMTxNumber 8 10
-        helper'  = do
+    let coin       = W.Coin {getColor: color, getCoin: coinAmount}
+        color      = W.Color {getC: 0}
+        coinAmount = W.CoinAmount "0.3242342"
+        key        = W.PublicKey "YblQ7+YCmxU/4InsOwSGH4Mm37zGjgy7CLrlWlnHdnM="
+        hash       = W.Hash "DldRwCblQ7Loqy6wYJnaodHl30d3j3eH+qtFzfEv46g="
+        addr       = W.Address { getAddress:  key }
+        tx         = W.TransactionSummarySerializable {txId: hash, txInputs: [tuple4 hash 0 coin $ Just addr], txOutputs: [(Tuple addr coin)], txInputsSum: [Tuple color coin], txOutputsSum: [Tuple color coin], txInputsTotal: coinAmount, txOutputsTotal: coinAmount }
+        err        = W.ParseError "error"
+        introMsg   = W.IMAddressInfo addr
+        aiMsg      = W.AIGetTransactions (Tuple 0 2)
+        outMsg     = W.OMTxNumber 8 10
+        helper'    = do
             helper coin
             helper key
             helper hash

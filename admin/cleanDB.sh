e28ebe3271ad91ede58cc2d7692a2cff051d9c06
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-07-30 00:10:10 +0300

    [RSC-95] Modify cleanDB script to cleanup everything
    Now `./admin/cleanDB.sh` script cleans up not only mintettes'
    local storages in `./admin/mintetteKeys` but also every `entity-db`
    in `~/.rscoin/`.

--- admin/cleanDB.sh
+++ admin/cleanDB.sh
@@ -3,3 +3,4 @@
 # dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 find . -iname "mintette-db" -print | xargs rm -rfv
+rm -rfv ~/.rscoin/{bank-db,mintette-db,wallet-db,notary-db,explorer-db}

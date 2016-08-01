bc419a8eac48d50b514a096c5818a9fa57dc1c96
Ante Kegalj <akegalj@gmail.com>
2016-08-01 19:23:56 +0200

    [RSC-108] Workaround

--- block-explorer/src/Config.purs
+++ block-explorer/src/Config.purs
@@ -1,6 +1,6 @@
 module App.Config where
 
-import Prelude (map, (<>), pure)
+import Prelude (map, (<>), pure, (==), bind)
 
 import Control.Bind ((>>=))
 import Control.Monad.Eff (Eff)
@@ -13,8 +13,16 @@ import DOM.HTML.Location (hostname) as L
 hostname :: forall eff. Eff (dom :: DOM | eff) String
 hostname = window >>= location >>= L.hostname
 
+-- FIXME: this is a workaround for RSC-108: https://issues.serokell.io/issue/RSC-108
 wsUrl :: forall eff. Eff (dom :: DOM | eff) String
-wsUrl = map (\h -> "ws://" <> h <> "/websocket") hostname
+wsUrl = do
+    hn <- hostname
+    if hn == "localhost"
+        then wsUrlDebug
+        else wsUrlProduction
+
+wsUrlProduction :: forall eff. Eff (dom :: DOM | eff) String
+wsUrlProduction = map (\h -> "ws://" <> h <> "/websocket") hostname
 
 wsUrlDebug :: forall eff. Eff (dom :: DOM | eff) String
 wsUrlDebug = pure "ws://localhost:8000"

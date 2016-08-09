fdd4b00dd0f2998146759c17d997d9e69eebdafc
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-08-09 17:58:12 +0300

    [RSC-130] Rewrite shell.nix to use haskell.buildStackProject

--- shell.nix
+++ shell.nix
@@ -1,22 +1,10 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "lts-6_7" }:
-
-let
-  inherit (nixpkgs) pkgs;
-  f = { mkDerivation, stdenv, pkgconfig }:
-      mkDerivation {
-        pname = "rscoin";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = true;
-        libraryPkgconfigDepends = 
-            with nixpkgs; [zlib glib cairo gnome.pango gnome3.gtk git 
-                           openssh autoreconfHook stack nodejs haskellPackages.purescript ];
-        license = stdenv.lib.licenses.gpl3;
-      };
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-  drv = haskellPackages.callPackage f { };
-in
-  if pkgs.lib.inNixShell then drv.env else drv
+let 
+  lts = "lts-6_7";
+in 
+  with import <nixpkgs> { };
+  haskell.lib.buildStackProject {
+     ghc = haskell.packages.${lts}.ghc;
+     name = "rscoin";
+     buildInputs = [ zlib glib cairo gnome.pango gnome3.gtk git cabal-install
+                     openssh autoreconfHook stack nodejs haskellPackages.purescript ];
+   }

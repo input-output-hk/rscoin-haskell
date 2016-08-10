108d1e0a75cbd0fc4c6676272d973bc751f21f37
Ante Kegalj <akegalj@gmail.com>
2016-08-10 16:58:59 +0200

    Remove gtk3 dependency

--- bench/install.sh
+++ bench/install.sh
@@ -6,7 +6,7 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 
 sudo apt-get update
-sudo aptitude install mc htop git pkg-config libgtk-3-dev
+sudo aptitude install mc htop git
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
 echo 'deb http://download.fpcomplete.com/ubuntu trusty main'|sudo tee /etc/apt/sources.list.d/fpco.list
 sudo apt-get update && sudo apt-get install stack -y

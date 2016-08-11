843f108f12fecc79b915c47d6d58e47f18df629f
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-08-11 03:50:22 +0100

    [RSC-133] Add keygen command line options

--- src/keygen/Main.hs
+++ src/keygen/Main.hs
@@ -1,24 +1,28 @@
+import           Data.Aeson          (encode)
+import           Data.ByteString     (writeFile)
+
 import           Options.Applicative (Parser, execParser, fullDesc, help,
                                       helper, info, metavar, progDesc,
                                       showDefault, strArgument, value, (<>))
 
+import           KeygenOptions       as Opts
 import           RSCoin.Core         (defaultSecretKeyPath, keyGen,
+                                      readSecretKey,
                                       writePublicKey, writeSecretKey)
-
-parser :: FilePath -> Parser FilePath
-parser def =
-    strArgument
-        (metavar "PATH" <> help "Path to store private key" <>
-         value def <> showDefault)
 
 main :: IO ()
 main = do
-    def <- defaultSecretKeyPath
-    fpName <-
-        execParser $
-        info (helper <*> parser def) (fullDesc <> progDesc "RSCoin's keygen")
-    let fpSecret = fpName <> ".sec"
-    let fpPublic = fpName <> ".pub"
-    (sk,pk) <- keyGen
-    writePublicKey fpPublic pk
-    writeSecretKey fpSecret sk
+    Opts.Options{..} <- Opts.getOptions
+    initLogging cloLogSeverity
+    case cloCommand of
+        Opts.Generate -> do
+            topSecretKey <- readSecretKey
+            let genNum = fromInteger cloPubKeyNum
+            keys <- replicateM genNum generator
+            let generatedKeys = encode keys
+            writeFile cloKeysPath generatedKeys
+  where generator = generatePKs cloKeysPath cloSkPath topSecretKey
+        generatePKs keyspath skpath topSK = do
+            (_, pk) <- keyGen
+            let sig = sign topSK pk
+            return (pk, sig)

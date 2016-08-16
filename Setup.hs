9f359ed52604dd4f96cd3c016004bc16bbe42b2a
Ante Kegalj <akegalj@gmail.com>
2016-08-16 02:31:58 +0200

    [RSC-90] Add frontend compilatoin flag
    Compiling with `stack build` won't compile frontend
    Compiling with `stack build --flag rscoin:befrontend` will compile
    frontend as well.

--- Setup.hs
+++ Setup.hs
@@ -1,7 +1,13 @@
-import           Distribution.Simple (UserHooks (postReg), defaultMainWithHooks,
-                                      simpleUserHooks)
-import           System.Exit         (ExitCode (..), exitWith)
-import           System.Process      (system)
+{-# LANGUAGE ViewPatterns #-}
+
+import           Control.Monad                      (when)
+import           Distribution.Simple                (UserHooks (postReg), defaultMainWithHooks,
+                                                     simpleUserHooks)
+import           Distribution.Simple.LocalBuildInfo (LocalBuildInfo (configFlags))
+import           Distribution.Simple.Setup          (ConfigFlags (configConfigurationsFlags))
+import           Distribution.PackageDescription    (FlagName (..))
+import           System.Exit                        (ExitCode (..), exitWith)
+import           System.Process                     (system)
 
 main :: IO ()
 main =
@@ -10,11 +16,12 @@ main =
     { postReg = hs2purs
     }
   where
-    hs2purs _ _ _ _ = do
-        system "stack $NIX_STACK exec -- rscoin-hs2purs" >>= printError
-        -- FIXME: https://gitlab.serokell.io/rscoin/rscoin/commit/fc8d36123dba122a4ce41053e1881ea5e1873030#note_1193
-        system
-            "cd block-explorer; PATH=$PATH:node_modules/.bin; npm install; npm run test; npm run build" >>=
-            printError
+    hs2purs _ _ _ (configFlags -> configConfigurationsFlags -> flags) =
+        when ((FlagName "befrontend", True) `elem` flags) $ do
+            system "stack $NIX_STACK exec -- rscoin-hs2purs" >>= printError
+            -- FIXME: https://gitlab.serokell.io/rscoin/rscoin/commit/fc8d36123dba122a4ce41053e1881ea5e1873030#note_1193
+            system
+                "cd block-explorer; PATH=$PATH:node_modules/.bin; npm install; npm run test; npm run build" >>=
+                printError
     printError ExitSuccess = return ()
     printError e = exitWith e

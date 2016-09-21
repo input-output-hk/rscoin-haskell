00ea04666160068fe335b374654b3e437ba10987
Ante Kegalj <akegalj@gmail.com>
2016-09-21 15:45:56 +0200

    [RSC-110] Fix date time format

--- block-explorer/src/Serokell/Data/DateTime.purs
+++ block-explorer/src/Serokell/Data/DateTime.purs
@@ -1,11 +1,12 @@
 module Serokell.Data.DateTime where
 
-import Prelude       ((<>), (<<<), ($), show)
+import Prelude       ((<>), (<<<), ($), show, (<=))
 
 import Data.DateTime (DateTime (..))
 import Data.Date 	 (day, month, year)
 import Data.Time 	 (hour, minute, second, millisecond)
 import Data.Enum     (fromEnum)
+import Data.String   (length)
 
 prettyDate :: DateTime -> String
 prettyDate = formatDate <<< toRecord
@@ -13,22 +14,28 @@ prettyDate = formatDate <<< toRecord
     formatDate dt = dt.year <> "-" <> dt.month <> "-" <> dt.day <> " " <> dt.hour <> ":" <> dt.minute <> ":" <> dt.second
 
 type DateRec =
-  { year :: String
-  , month :: String
-  , day :: String
-  , hour :: String
-  , minute :: String
-  , second :: String
-  , millisecond :: String
-}
+    { year :: String
+    , month :: String
+    , day :: String
+    , hour :: String
+    , minute :: String
+    , second :: String
+    , millisecond :: String
+    }
 
 toRecord :: DateTime -> DateRec
 toRecord (DateTime d t) =
-  { year: show $ fromEnum (year d)
-  , month: show $ fromEnum (month d)
-  , day: show $ fromEnum (day d)
-  , hour: show $ fromEnum (hour t)
-  , minute: show $ fromEnum (minute t)
-  , second: show $ fromEnum (second t)
-  , millisecond: show $ fromEnum (millisecond t)
-}
+    { year: show $ fromEnum (year d)
+    , month: paddZero $ show $ fromEnum (month d)
+    , day: paddZero $ show $ fromEnum (day d)
+    , hour: paddZero $ show $ fromEnum (hour t)
+    , minute: paddZero $ show $ fromEnum (minute t)
+    , second: paddZero $ show $ fromEnum (second t)
+    , millisecond: paddZero $ show $ fromEnum (millisecond t)
+    }
+  where
+    -- NOTE: this is assuming `length s <= 2`
+    paddZero s =
+        if length s <= 1
+            then "0" <> s
+            else s

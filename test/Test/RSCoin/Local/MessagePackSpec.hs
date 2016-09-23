78c0b6e76c97bbd872a48d2a2b29a71e1bdae451
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-09-23 21:27:16 +0100

    [RSC-224] Refactor MessagePack identity tests; Bump 'rscoin-core'

--- test/Test/RSCoin/Local/MessagePackSpec.hs
+++ test/Test/RSCoin/Local/MessagePackSpec.hs
@@ -4,11 +4,11 @@ module Test.RSCoin.Local.MessagePackSpec
        ( spec
        ) where
 
-import           Data.Maybe                  (fromJust)
 import           Data.MessagePack            (MessagePack (..), pack, unpack)
+import           Data.Proxy                  (Proxy (Proxy))
 import           Test.Hspec                  (Spec, describe)
 import           Test.Hspec.QuickCheck       (prop)
-import           Test.QuickCheck             ((===))
+import           Test.QuickCheck             (Arbitrary, (===))
 
 import qualified RSCoin.Core                 as C
 import           RSCoin.Mintette.Error       (MintetteError)
@@ -20,12 +20,18 @@ spec :: Spec
 spec =
     describe "MessagePack" $
         describe "Identity Properties" $ do
-            prop "Either MintetteError CheckConfirmation" $
-                \(a :: Either MintetteError C.CheckConfirmation) -> a === mid a
-            prop "MintetteError" $
-                \(a :: MintetteError) -> a === mid a
-            prop "NotaryError" $
-                \(a :: NotaryError) -> a === mid a
+            makeMsgPackProp "Either MintetteError CheckConfirmation"
+                (Proxy :: Proxy (Either MintetteError C.CheckConfirmation))
+            makeMsgPackProp "MintetteError" (Proxy :: Proxy MintetteError)
+            makeMsgPackProp "NotaryError" (Proxy :: Proxy NotaryError)
+
+makeMsgPackProp
+    :: forall a.
+       (Show a, Eq a, MessagePack a, Arbitrary a)
+    => String -> Proxy a -> Spec
+makeMsgPackProp s Proxy = prop s $ \(x :: a) -> x === mid x
 
 mid :: MessagePack a => a -> a
-mid = fromJust . unpack . pack
+mid = maybe err id . unpack . pack
+  where
+    err = error "[MessagePackSpec] Failed MessagePack decoding"

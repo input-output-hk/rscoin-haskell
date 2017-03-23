109d8f6f226e9d0b360fcaac14c5a90da112a810
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-03-23 18:41:05 +0300

    Make it compile w/ nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 let 
-  lts = "lts-6_7";
+  lts = "ghc7103";
 in 
   with import <nixpkgs> { };
   haskell.lib.buildStackProject {
      ghc = haskell.packages.${lts}.ghc;
      name = "rscoin";
      buildInputs = [ zlib glib cairo gnome2.pango gnome3.gtk git cabal-install
-                     openssh autoreconfHook stack nodejs haskellPackages.purescript ];
+                     openssh autoreconfHook stack nodejs gmp ];
    }
